3a86648ded06bb58628bfc8d344dde19711b33ba
Alexander Levin <alevin.ru@gmail.com>
2016-03-03 21:35:49 +0200

    define Article model using iosAdapter

--- src/app/models/model.module.js
+++ src/app/models/model.module.js
@@ -3,54 +3,20 @@
 (function () {
 
   angular.module('webPage')
-    .service('models',function(DS, DSUtils, $window, $log){
+    .service('models',function(DS, IosAdapter, $log){
 
-      function MyCustomAdapter() {
+      DS.registerAdapter('ios', new IosAdapter(), {default: true});
 
-      }
+      var Article = DS.defineResource('Article');
 
-      MyCustomAdapter.prototype.findAll = function () {
-        // Must return a promise that resolves with the found items
-        return new DSUtils.Promise(function (resolve) {
-          $log.log('yeah!');
-          if ($window.webkit) {
-            $window.webkit.messageHandlers.findAll.postMessage({
-              entity: 'Article',
-              options: {
-                requestId: 1,
-                pageSize: 10,
-                startPage: 1
-              }
-            });
-          } else {
-            $log.log('webkit not defined');
-          }
-          resolve();
-        });
+      $log.log (Article);
+
+      return {
+        Article: Article
       };
-
-      var id = 2;
-
-      MyCustomAdapter.prototype.create = function (definition, attrs) {
-        return new DSUtils.Promise(function (resolve) {
-          attrs.id = id++;
-          resolve (attrs);
-        });
-      };
-
-      DS.registerAdapter('my', new MyCustomAdapter(), {default: true});
-
-      var entity = DS.defineResource('entity');
-
-      $window.iSistemiumIOSCallback = function (data, options) {
-        DS.inject('entity',angular.extend ({id:1},data));
-        return entity;
-      };
-
-      return entity;
 
     }).run (function (models,$log){
-      models.findAll().then(function () {
+      models.Article.findAll().then(function () {
         $log.log('findAll resolved');
       });
     });
