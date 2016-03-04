3e1883e11f7a6b104be31c21cee63b62e499e397
Albert <kovalevskij.albert@gmail.com>
2016-03-04 17:56:22 +0200

    article list

---
+++ src/app/domain/picking/articleList.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+
+  angular.module('webPage')
+    .controller('ArticleListController', function ($scope, models) {
+
+      var vm = this;
+      vm.articles = [];
+
+      if ($scope.vm.selectedItems) {
+        $scope.vm.selectedItems.forEach(function (i) {
+          console.log(i);
+          i.PickingOrderPositions.forEach(function (p) {
+            models.PickingOrderPosition.loadRelations(p).then(function (a) {
+              console.log(a);
+              vm.articles.push(a.Article);
+            })
+          });
+        });
+      }
+
+    })
+  ;
+
+}());
