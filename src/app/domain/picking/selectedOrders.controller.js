82f69864fbf0359978465571a27adc8fb8d028df
Albert <kovalevskij.albert@gmail.com>
2016-03-04 17:55:48 +0200

    selected orders

---
+++ src/app/domain/picking/selectedOrders.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+
+  angular.module('webPage')
+    .controller('SelectedOrdersController', function ($state, $scope) {
+
+      if (!$scope.vm.selectedItems.length) {
+        $state.go('picking.pickingOrderList');
+      }
+
+      var vm = this;
+      vm.total = 0;
+      vm.selectedItems = $scope.vm.selectedItems;
+      vm.selectedItems.forEach(function (i) {
+        vm.total += i.PickingOrderPositions.length;
+      });
+
+    })
+  ;
+
+}());
