666e2670d1e09206b2e0f548e621237b71ccf68f
Alexander Levin <alevin.ru@gmail.com>
2016-03-04 20:09:00 +0200

    rewrite routes using angular-ui-router.stateHelper

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -3,30 +3,39 @@
 (function () {
 
   angular.module('webPage')
-    .config(function ($stateProvider) {
+    .config(function (stateHelperProvider) {
 
-      $stateProvider
-        .state('picking', {
+      stateHelperProvider
+        .state({
+          name: 'picking',
           abstract: true,
-          template: '<div class="container"><acme-navbar></acme-navbar><div ui-view=""></div></div>'
-        })
-        .state('picking.pickingOrderList', {
-          url: '/orders',
-          templateUrl: 'app/domain/picking/pickingOrderList.html',
-          controller: 'PickingOrderListController',
-          controllerAs: 'vm'
-        })
-        .state('picking.pickingOrderList.selectedOrders', {
-          url: '/selected',
-          templateUrl: 'app/domain/picking/selectedOrders.html',
-          controller: 'SelectedOrdersController',
-          controllerAs: 'vm'
-        })
-        .state('picking.pickingOrderList.selectedOrders.articleList', {
-          url: '/articleList',
-          templateUrl: 'app/domain/picking/articleList.html',
-          controller: 'ArticleListController',
-          controllerAs: 'vm'
+          template: '<div class="container"><acme-navbar></acme-navbar><div ui-view=""></div></div>',
+          children: [
+            { name:'pickingOrderList',
+              url: '/orders',
+              templateUrl: 'app/domain/picking/pickingOrderList.html',
+              controller: 'PickingOrderListController',
+              controllerAs: 'vm',
+              children: [
+                {
+                  name: 'selectedOrders',
+                  url: '/selected',
+                  templateUrl: 'app/domain/picking/selectedOrders.html',
+                  controller: 'SelectedOrdersController',
+                  controllerAs: 'vm',
+                  children: [
+                    {
+                      name: 'articleList',
+                      url: '/articleList',
+                      templateUrl: 'app/domain/picking/articleList.html',
+                      controller: 'ArticleListController',
+                      controllerAs: 'vm'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
         })
       ;
 
