1c8431f5a2992c40d291ff0d2e66c7c2a5cf0668
Albert <kovalevskij.albert@gmail.com>
2016-03-04 14:29:34 +0200

    models

--- src/app/models/model.module.js
+++ src/app/models/model.module.js
@@ -3,9 +3,37 @@
 (function () {
 
   angular.module('webPage')
-    .service('models',function(DS, IosAdapter, $log){
+    .config(function (DSProvider, DSHttpAdapterProvider) {
+      angular.extend(DSHttpAdapterProvider.defaults, {
+        basePath: 'http://localhost:9000/api/dev/',
+        httpConfig: {
+          headers: {
+            'X-Return-Post': 'true',
+            'authorization': '9c32b54d43f12d9ef65a58193d0e98d2@pha'
+          }
+        },
+        queryTransform: function queryTransform(resourceConfig, params) {
+          var res = {};
 
-      DS.registerAdapter('ios', new IosAdapter(), {default: true});
+          if (params.offset) {
+            res['x-start-page:'] = Math.ceil(params.offset/params.limit);
+          }
+          if (params.limit) {
+            res['x-page-size:'] = params.limit;
+          }
+
+          delete params.limit;
+          delete params.offset;
+
+          return angular.extend (res, params);
+        }
+      });
+    })
+    .service('models',function(DS, IosAdapter, $window, $log){
+
+      if ($window.webkit) {
+        DS.registerAdapter('ios', new IosAdapter(), {default: true});
+      }
 
       var Article = DS.defineResource({
         name: 'Article',
@@ -31,9 +59,73 @@
         }
       });
 
+      var PickingOrder = DS.defineResource({
+        name: 'PickingOrder',
+        relations: {
+          hasMany: {
+            PickingOrderPosition: {
+              localField: 'PickingOrders',
+              foreignKey: 'pickingOrderPosition'
+            }
+          }
+        }
+      });
+
+      var PickingOrderPosition = DS.defineResource({
+        name: 'PickingOrderPosition',
+        relations: {
+          belongsTo: {
+            PickingOrder: {
+              localField: 'PickingOrder',
+              localKey: 'pickingOrder'
+            }
+          },
+          hasOne: {
+            Article: {
+              localField: 'Article',
+              localKey: 'article'
+            }
+          }
+        }
+      });
+
+      var StockBatch = DS.defineResource({
+        name: 'StockBatch',
+        relations: {
+          hasOne: {
+            Article: {
+              localField: 'Article',
+              localKey: 'article'
+            }
+          },
+          hasMany: {
+            StockBatchBarcode: {
+              localField: 'StockBatchBarcode',
+              localKey: 'stockBatchBarcode'
+            }
+          }
+        }
+      });
+
+      var StockBatchBarcode = DS.defineResource({
+        name: 'StockBatchBarcode',
+        relations: {
+          belongsTo: {
+            StockBatch: {
+              localField: 'StockBatch',
+              localKey: 'stockBatch'
+            }
+          }
+        }
+      });
+
       var schema = {
         Article: Article,
-        ArticleGroup: ArticleGroup
+        ArticleGroup: ArticleGroup,
+        PickingOrder: PickingOrder,
+        PickingOrderPosition: PickingOrderPosition,
+        StockBatch: StockBatch,
+        StockBatchBarcode: StockBatchBarcode
       };
 
       $log.log (schema);
