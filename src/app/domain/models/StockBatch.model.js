8f579b6d332eb2cb1e19809d90b9bce7a1c997f5
Alexander Levin <alevin.ru@gmail.com>
2016-03-06 20:04:38 +0200

    StockBatch.someBy.barCode (code)

--- src/app/domain/models/StockBatch.model.js
+++ src/app/domain/models/StockBatch.model.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema) {
+    angular.module('Models').run(function (Schema, $q, $timeout) {
 
       Schema.register ({
 
@@ -20,6 +20,45 @@
               localField: 'StockBatchBarCodes',
               foreignKey: 'stockBatch'
             }
+          }
+        },
+
+        someBy: {
+
+          barCode: function (code) {
+
+            var SBBC = Schema.model('StockBatchBarCode');
+            var SB = Schema.model('StockBatch');
+
+            return $q (function (resolve,reject){
+
+              if (!code) {
+                reject ('Укажите штрих-код');
+              }
+
+              SBBC.findAll({
+                code: code
+              }).then(function (res) {
+
+                if (!res.length) {
+                  return resolve ([]);
+                }
+
+                var qs = _.map(res, function (i) {
+                  return SBBC.loadRelations(i);
+                });
+
+                $q.all(qs).then (function (sbbcs){
+
+                  $q.all(_.map (sbbcs,function (sbbc) {
+                    return SB.loadRelations(sbbc.stockBatch,'Article');
+                  })).then (resolve,reject);
+
+                },reject);
+
+              });
+
+            });
           }
         }
 
