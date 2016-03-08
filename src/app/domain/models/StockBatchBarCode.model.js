b4f2aaec3007d2c5fa94bbbd541a0f6827d1e04b
Alexander Levin <alevin.ru@gmail.com>
2016-03-08 09:07:23 +0200

    StockBatchBarCode.someBy.article

--- src/app/domain/models/StockBatchBarCode.model.js
+++ src/app/domain/models/StockBatchBarCode.model.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema) {
+    angular.module('Models').run(function (Schema,$q) {
 
       Schema.register ({
 
@@ -15,6 +15,34 @@
               localKey: 'stockBatch'
             }
           }
+        },
+
+        someBy: {
+
+          article: function (id) {
+
+            var SB = Schema.model('StockBatch');
+
+            return $q(function (resolve, reject) {
+
+              if (!id) {
+                return reject('Укажите товар');
+              }
+
+              SB.findAll({article: id, limit: 1}).then(function (sbs) {
+                if (!sbs.length) {
+                  return reject ();
+                }
+                SB.loadRelations(sbs[0], 'StockBatchBarCode')
+                  .then(function (sb) {
+                    resolve(sb.StockBatchBarCodes);
+                  }, reject);
+              }, reject);
+
+            });
+
+          }
+
         }
 
       });
