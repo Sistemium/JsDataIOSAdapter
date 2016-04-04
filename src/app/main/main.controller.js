5cebfbe7b4c9bab309fa58af789a28c4f5a183c5
Alexander Levin <alevin.ru@gmail.com>
2016-04-04 14:53:09 +0300

    remove demo files

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,35 +6,11 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr, Auth, Menu) {
+  function MainController(Auth, Menu) {
     var vm = this;
 
     vm.Auth = Auth;
     vm.data = Menu.root();
 
-    vm.classAnimation = '';
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function () {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function (awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
