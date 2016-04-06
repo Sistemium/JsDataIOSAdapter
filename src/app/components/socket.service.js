9e9f98a6655ea57a6bbb4b567f78c747506469d9
Albert <kovalevskij.albert@gmail.com>
2016-04-06 13:22:33 +0300

    socket service

---
+++ src/app/components/socket.service.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('webPage')
+  .factory('Sockets', function($rootScope, $q) {
+
+    var url = 'http://localhost:8000/';
+
+    var socket = io(url, {
+      path: '/socket.io-client'
+    });
+
+    var svc = {
+      io: socket,
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+
+        if ((angular.isFunction(data)) && !callback) {
+          socket.emit(eventName, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (data) {
+                data.apply(socket, args);
+              }
+            });
+          });
+        } else {
+          socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+                callback.apply(socket, args);
+              }
+            });
+          });
+        }
+      },
+      emitQ: function (eventName, data) {
+
+        var q = $q.defer();
+
+        svc.emit(eventName, data, function (data){
+          if (data) {
+            q.resolve (data);
+          } else {
+            q.reject ();
+          }
+        });
+
+        return q.promise;
+      },
+      removeAllListeners: socket.removeAllListeners,
+      removeListener: socket.removeListener
+    };
+
+    return svc;
+
+  });
