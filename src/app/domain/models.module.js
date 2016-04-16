68fcae99abb805e150908365d3113a4acf3d2c5b
Alexander Levin <alevin.ru@gmail.com>
2016-04-16 18:27:27 +0300

    us saSchema function

--- src/app/domain/models.module.js
+++ src/app/domain/models.module.js
@@ -2,38 +2,60 @@
 
 (function () {
 
-  //inject only models module
+  var basePath = window.localStorage.getItem('JSData.BasePath')
+    || location.protocol === 'https:' && '/api/dev/'
+    || 'https://api.sistemium.com/v4d/dev/'
+  ;
+
   angular.module('Models', ['sistemium'])
 
     .config(function (DSHttpAdapterProvider) {
-      var basePath = window.localStorage.getItem('JSData.BasePath')
-        || location.protocol === 'https:' && '/api/dev/'
-        || 'https://api.sistemium.com/v4d/dev/';
       angular.extend(DSHttpAdapterProvider.defaults, {
         basePath: basePath
       });
     })
 
+    .service('Schema', Schema)
+
     .service('models', function (Schema) {
-
       return Schema.models();
-
     })
 
-    .service('Schema', function(saSchema) {
-      return saSchema;
-    })
-
-    .run(function($window, DS, IosAdapter, SocketAdapter, Schema){
-
-      if ($window.webkit) {
-        DS.registerAdapter('ios', new IosAdapter (Schema), {default: true});
-      } else {
-        DS.registerAdapter('socket', new SocketAdapter(), {default: true});
-      }
-
-    })
-
+    .run (registerAdapters)
   ;
+
+  function Schema (saSchema,$http) {
+    return saSchema({
+      getCount: function (resource,params) {
+
+        return $http
+
+          .get(
+            basePath + '/' + resource.endpoint,
+            {
+              params: angular.extend({
+                'agg:': 'count'
+              }, params || {})
+            }
+          )
+
+          .then(function (res) {
+            return parseInt(res.headers('x-aggregate-count'));
+          })
+          ;
+
+      }
+    });
+  }
+
+  function registerAdapters ($window, DS, IosAdapter, SocketAdapter, Schema) {
+
+    if ($window.webkit) {
+      DS.registerAdapter('ios', new IosAdapter (Schema), {default: true});
+    } else {
+      DS.registerAdapter('socket', new SocketAdapter(), {default: true});
+    }
+
+  }
 
 }());
