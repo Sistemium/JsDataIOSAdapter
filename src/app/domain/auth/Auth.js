ba4462af9544feb91e786523b078441fa68bcee6
Alexander Levin <alevin.ru@gmail.com>
2016-06-04 09:57:53 +0300

    check roles on init fix

--- src/app/domain/auth/Auth.js
+++ src/app/domain/auth/Auth.js
@@ -2,49 +2,78 @@
 
 (function () {
 
-  angular.module('webPage').service('Auth', function ($rootScope, $q, $state, Sockets, $window) {
+  angular.module('webPage').service('Auth', function ($rootScope, $q, $state, Sockets, $window, IOS) {
 
     var me = this;
     var currentUser;
     var DEBUG = debug ('stg:Auth');
     var roles;
     var rolesPromise;
+    var ios = IOS.isIos();
+    var resolveRoles;
 
     function getAccessToken() {
-      return !!$window.webkit || $window.localStorage.getItem('authorization');
+      return ios || $window.localStorage.getItem('authorization');
     }
 
     function init(authProtocol) {
+
       if (rolesPromise) {
         return rolesPromise;
       }
+
       var token = getAccessToken();
-      if (token && !roles) {
+
+      if (token && !roles || ios) {
+
         rolesPromise = authProtocol.getRoles(token)
           .then(function(res){
-            //roles = res.roles;
+            roles = res;
             console.log ('Auth.init',res);
             return res;
           });
+
         return rolesPromise;
+
       } else if (roles) {
-        return $q(function(resolve){
+
+        rolesPromise = $q(function(resolve){
           resolve(roles);
         });
+
+        return rolesPromise;
+
+      } else {
+
+        var rolesPromise1 = $q(function(resolve){
+          resolveRoles = resolve;
+        });
+
+        return rolesPromise1;
+
       }
+
     }
 
     var needAuth = $rootScope.$on('$stateChangeStart', function (event, next, nextParams) {
 
-      if (!roles && next.name !== 'auth') {
-        event.preventDefault();
-        if (rolesPromise) {
-          rolesPromise.then(function(){
-            $state.go(next,nextParams);
-          });
+      if (!roles) {
+        if (next.name !== 'auth') {
+
+          event.preventDefault();
+          if (rolesPromise) {
+            rolesPromise.then(function () {
+              $state.go(next, nextParams);
+            });
+            return;
+          }
+
+          $state.go('auth');
+          return;
+
+        } else {
           return;
         }
-        return $state.go('auth');
       }
 
       var needRoles = next.data && next.data.auth;
@@ -61,7 +90,11 @@
     });
 
     var onAuthenticated = $rootScope.$on('authenticated', function (event, res) {
-      roles = res.roles;
+      console.log ('authenticated', res);
+      roles = res;
+      if (resolveRoles) {
+        resolveRoles (roles);
+      }
       $window.localStorage.setItem('authorization', res.accessToken);
       sockAuth();
     });
