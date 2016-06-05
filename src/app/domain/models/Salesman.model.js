58bf050f8b8713efce3354d1886937b71aeaf0cf
Alexander Levin <alevin.ru@gmail.com>
2016-06-05 20:49:56 +0300

    Salesman.model

---
+++ src/app/domain/models/Salesman.model.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+
+  angular.module('Models').run(function (Schema) {
+
+    Schema.register({
+
+      name: 'Salesman',
+
+      labels: {
+        multiple: 'Торговые представители',
+        single: 'Торговый представитель'
+      },
+
+      relations: {
+        hasMany: {
+          // SaleOrder: {
+          //   localField: 'saleOrders',
+          //   foreignKey: 'salesmanId'
+          // },
+          PrePreOrder: {
+            localField: 'prePreOrders',
+            foreignKey: 'salesmanId'
+          }
+        }
+      },
+
+      computed: {
+        shortName: ['name', function (name) {
+          var m = name.match (/^[^ ]+ [^ ]+/);
+          return m ? m[0] : null;
+        }]
+      },
+
+      getCurrent: function () {
+        return;
+      }
+
+    });
+
+  });
+
+})();
