e5115727a04d7f03fe07dc03b1ab5d1387ee8fc6
Alexander Levin <alevin.ru@gmail.com>
2016-06-11 21:03:28 +0300

    Location endpoint CoreLocation

--- src/app/domain/models/Location.model.js
+++ src/app/domain/models/Location.model.js
@@ -7,10 +7,25 @@
     Schema.register({
 
       name: 'Location',
+      endpoint: 'CoreLocation',
 
       labels: {
         multiple: 'Геометки',
         single: 'Геометка'
+      },
+
+      relations: {
+        hasMany: {
+          Location: [
+            {
+              localField: 'visitIn',
+              foreignKey: 'checkInLocationId'
+            },{
+              localField: 'visitOut',
+              foreignKey: 'checkOutLocationId'
+            }
+          ]
+        }
       }
 
     });
