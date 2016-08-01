1763a38958a2c305c790d25269c1279d224ff782
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-08-01 18:32:56 +0300

    save new partner and outlet

--- src/app/domain/sales/ctrl/AddOutletController.js
+++ src/app/domain/sales/ctrl/AddOutletController.js
@@ -2,10 +2,11 @@
 
 (function () {
 
-  function AddOutletController($scope, $state, ConfirmModal, Schema) {
+  function AddOutletController($scope, $state, $q, ConfirmModal, Schema, toastr) {
 
     var vm = this;
     var Partner = Schema.model('Partner');
+    var Outlet = Schema.model('Outlet');
 
     function refresh() {
 
@@ -21,8 +22,73 @@
       ConfirmModal.show({
         text: 'Сохранить точку?'
       })
-        .then(quit)
-      ;
+      .then(saveNewData);
+
+    }
+
+    function saveNewData() {
+
+      vm.busy = $q(function (resolve, reject) {
+
+        if (!vm.selectedPartner) {
+
+          var newPartner = Partner.inject({
+            name: vm.name
+          });
+
+          Partner.save(newPartner)
+            .then(function(partner){
+
+              resolve(partner);
+              saveOutlet(vm.name, partner)
+                .then(function(outlet){
+
+                  resolve(outlet);
+                  quit();
+
+                }, function (err) {
+
+                  reject(err);
+                  toastr.error(angular.toJson(err), 'Не удалось сохранить точку');
+
+                });
+
+            }, function (err) {
+
+              reject(err);
+              toastr.error(angular.toJson(err), 'Не удалось сохранить партнёра');
+
+            });
+
+        } else {
+
+          saveOutlet(vm.name, vm.selectedPartner)
+            .then(function(outlet){
+
+              resolve(outlet);
+              quit();
+
+            }, function (err) {
+
+              reject(err);
+              toastr.error(angular.toJson(err), 'Не удалось сохранить точку');
+
+            });
+        }
+
+      }).then(quit);
+
+    }
+
+    function saveOutlet(name, partner) {
+
+      var newOutlet = Outlet.inject({
+        address: vm.address,
+        name: name,
+        partnerId: partner.id
+      });
+
+      return Outlet.save(newOutlet);
 
     }
 
