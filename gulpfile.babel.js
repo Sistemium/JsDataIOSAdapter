388c7c4b4bb6856ec53759c636a99b3f771a034d
Alexander Levin <alevin.ru@gmail.com>
2016-08-07 14:08:40 +0300

    use sistemium-gulp custom babel-register

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,16 @@ var config = {
   base: 'node_modules/sistemium-gulp'
 };
 
+require('babel-register')({
+  ignore: function(filename) {
+    if (/node_modules\/sistemium-gulp/.test(filename)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+});
+
 var gulp = require('gulp');
 
 require('sistemium-gulp')
