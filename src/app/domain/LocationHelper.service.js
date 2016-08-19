c5fb5640df055f33bc2c2c0529dff59c2e82ca25
Alexander Levin <alevin.ru@gmail.com>
2016-08-19 14:37:48 +0300

    LocationHelper working in browser

--- src/app/domain/LocationHelper.service.js
+++ src/app/domain/LocationHelper.service.js
@@ -2,14 +2,75 @@
 
 (function () {
 
-  function LocationHelper(IOS) {
+  function LocationHelper(IOS, $window, $q, Schema) {
+
+    function browserGetLocation(accuracy) {
+
+      var geo = $window.navigator.geolocation;
+
+      return $q(function (resolve, reject) {
+
+        var geoOptions = {
+          enableHighAccuracy: true,
+          maximumAge: 30000,
+          timeout: 27000
+        };
+
+        function successWatch(location) {
+          if (_.get(location, 'coords.accuracy') <= accuracy) {
+            geo.clearWatch(watchID);
+            /*
+             {
+             accuracy: 65,
+             altitude: null,
+             altitudeAccuracy: null,
+             heading: null,
+             latitude: 54.69379157800531,
+             longitude: 25.268661268375816,
+             speed: null
+             },
+             timestamp: 493297545792
+             */
+            var coords = location.coords;
+            var res = {
+              horizontalAccuracy: coords.accuracy,
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+              timestamp: moment().utc().format('YYYY-MM-DD HH:mm:ss.SSS')
+            };
+
+            resolve(res);
+          }
+        }
+
+        function failWatch(err) {
+          geo.clearWatch(watchID);
+          reject(err);
+        }
+
+        var watchID = geo.watchPosition(successWatch, failWatch, geoOptions);
+
+      });
+
+    }
 
     function getLocation(accuracy, ownerXid, target) {
 
-      return IOS.checkIn(accuracy, {
+      var initData = {
         ownerXid: ownerXid,
         target: target
-      });
+      };
+
+      if (IOS.isIos()) {
+        return IOS.checkIn(accuracy, initData);
+      } else {
+        return browserGetLocation(accuracy)
+          .then(function (location) {
+            var Location = Schema.model('Location');
+            return Location.create(_.assign(location, initData));
+          })
+      }
+
 
     }
 
