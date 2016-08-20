ba57cc0d7bff8d9990408498317aaea0c7cc5269
Alexander Levin <alevin.ru@gmail.com>
2016-08-20 22:10:23 +0300

    commented reload counter in index.run

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -7,7 +7,8 @@
 
   /** @ngInject */
   function runBlock() {
-
+    // uncomment to check how often browser reloads
+    // localStorage.setItem('cnt', parseInt(localStorage.getItem('cnt') || 0) + 1);
   }
 
 })();
