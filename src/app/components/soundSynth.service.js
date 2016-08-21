ad0e64ce6b2c153e5d45d12dfd4a938fab59bbd9
Alexander Levin <alevin.ru@gmail.com>
2016-08-21 09:22:36 +0300

    autoformat + ES6

--- src/app/components/soundSynth.service.js
+++ src/app/components/soundSynth.service.js
@@ -2,72 +2,68 @@
 
 (function () {
 
-    angular.module('core.services').service('SoundSynth', function ($window, toastr, $q, IOS) {
+  angular.module('core.services').service('SoundSynth', function ($window, toastr, $q, IOS) {
 
-      var rate = 0.4;
-      var pitch = 1;
-      var lastSpeech = false;
-      var speakerCallBackFn = 'speakerCallBack';
-      var promises = {};
-      var id = 1;
+    var rate = 0.4;
+    var pitch = 1;
+    var lastSpeech = false;
+    var speakerCallBackFn = 'speakerCallBack';
+    var promises = {};
+    var id = 1;
 
-      function speakerCallBack () {
-        _.each (promises,function (promise, id) {
-          promise.resolve();
-          delete promises[id];
-        });
-      }
-
-      $window[speakerCallBackFn] = speakerCallBack;
-
-      IOS.getDevicePlatform().then(function (devicePlatform) {
-        if (/iPhone/.test(devicePlatform)) {
-          rate = 0.2;
-        }
+    function speakerCallBack() {
+      _.each(promises, function (promise, id) {
+        promise.resolve();
+        delete promises[id];
       });
+    }
 
-      function speaker (text) {
-        return $q(function(resolve){
+    $window[speakerCallBackFn] = speakerCallBack;
 
-          promises [id] = {
-            resolve: resolve
-          };
-
-          $window.webkit.messageHandlers.sound.postMessage({
-            text: text.replace(/[^а-я0-9,]/ig,' '),
-            rate: rate,
-            pitch: pitch,
-            callBack: speakerCallBackFn,
-            options: {
-              requestId: id++
-            }
-          });
-
-        });
+    IOS.getDevicePlatform().then(function (devicePlatform) {
+      if (/iPhone/.test(devicePlatform)) {
+        rate = 0.2;
       }
-
-      function mockSpeaker (text) {
-        return $q(function (resolve){
-          toastr.success (text);
-          resolve();
-        });
-      }
-
-      function say (text) {
-        var sp = $window.webkit ? speaker : mockSpeaker;
-        lastSpeech = text;
-        return sp (text);
-      }
-
-      return {
-
-        say: say,
-        repeat: function () {
-          say (lastSpeech);
-        }
-
-      };
-
     });
+
+    function speaker(text) {
+      return $q(function (resolve) {
+
+        promises [id] = {
+          resolve: resolve
+        };
+
+        $window.webkit.messageHandlers.sound.postMessage({
+          text: text.replace(/[^а-я0-9,]/ig, ' '),
+          rate: rate,
+          pitch: pitch,
+          callBack: speakerCallBackFn,
+          options: {
+            requestId: id++
+          }
+        });
+
+      });
+    }
+
+    function mockSpeaker(text) {
+      return $q(function (resolve) {
+        toastr.success(text);
+        resolve();
+      });
+    }
+
+    function say(text) {
+      var sp = $window.webkit ? speaker : mockSpeaker;
+      lastSpeech = text;
+      return sp(text);
+    }
+
+    return {
+      say,
+      repeat: () => say(lastSpeech)
+    };
+
+  });
 
 })();
