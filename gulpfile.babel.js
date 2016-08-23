46a54a59853e93b247849516e6ca664538de5669
Alexander Levin <alevin.ru@gmail.com>
2016-08-23 18:57:01 +0300

    use sistemium-gulp 0.2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,7 @@
 'use strict';
 
-var config = {
-  base: 'node_modules/sistemium-gulp'
-};
-
-require('babel-register')({
-  ignore: function(filename) {
-    return !/sistemium-gulp\/(gulp|[^\/]*js)/.test(filename);
-  }
-});
-
-var gulp = require('gulp');
-
 require('sistemium-gulp')
-  .run(gulp, config);
+  .config({
+    ngModule: 'webPage'
+  })
+  .run(require('gulp'));
