7cd7be76e3d418c3f2c3c83334a67c942bbef6c0
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-07 15:12:12 +0300

    add EditOutlet.controller.js

---
+++ src/app/domain/sales/ctrl/EditOutlet.controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  function EditOutletController() {
+
+    var vm = this;
+
+    _.assign(vm, {
+    });
+
+  }
+
+  angular.module('webPage')
+    .controller('EditOutletController', EditOutletController);
+
+})
+();
