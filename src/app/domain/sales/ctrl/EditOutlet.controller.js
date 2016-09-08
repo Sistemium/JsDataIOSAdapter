314945c8e602bbcc1c5238c55c66e723da979573
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 10:18:16 +0300

    add findOutlet and selectPartner functions in EditOutlet.controller.js

--- src/app/domain/sales/ctrl/EditOutlet.controller.js
+++ src/app/domain/sales/ctrl/EditOutlet.controller.js
@@ -2,12 +2,54 @@
 
 (function () {
 
-  function EditOutletController() {
+  function EditOutletController(Schema, $state) {
 
     var vm = this;
 
     _.assign(vm, {
+
+      busy: null,
+
+      outlet: null,
+      partner: null,
+      partners: [],
+      selectedPartner: null,
+
+      address: '',
+
+      selectPartner
+
     });
+
+    var Outlet = Schema.model('Outlet');
+    var Partner = Schema.model('Partner');
+
+    vm.busy = findOutlet();
+
+    function findOutlet() {
+
+      return Outlet.find($state.params.id)
+        .then(function (outlet) {
+
+          vm.outlet = outlet;
+          vm.address = outlet.address;
+
+          return Partner.findAll()
+            .then(function (partners) {
+
+              vm.partners = _.sortBy(partners, (p) => [_.toLower(p.shortName), _.toLower(p.name)]);
+              vm.partner = _.find(partners, {id: outlet.partnerId});
+
+            });
+
+        });
+
+    }
+
+    function selectPartner(partner) {
+      vm.selectedPartner = partner;
+    }
+
 
   }
 
