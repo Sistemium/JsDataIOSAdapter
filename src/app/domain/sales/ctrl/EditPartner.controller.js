95c4fd267cc31ac1c38b272dd65a655ea15363b3
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 17:08:03 +0300

    EditPartner.controller.js

---
+++ src/app/domain/sales/ctrl/EditPartner.controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  function EditPartnerController() {
+
+    var vm = this;
+
+    _.assign(vm, {
+    });
+
+  }
+
+  angular.module('webPage')
+    .controller('EditPartnerController', EditPartnerController);
+
+})
+();
