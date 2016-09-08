450ced98d6efddbac4c2fc4eda74e3d3eab97137
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 11:10:18 +0300

    save new outlet data EditOutlet.controller.js

--- src/app/domain/sales/ctrl/EditOutlet.controller.js
+++ src/app/domain/sales/ctrl/EditOutlet.controller.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  function EditOutletController(Schema, $state/*, $scope*/) {
+  function EditOutletController(Schema, $state, $window, ConfirmModal/*, $scope*/) {
 
     var vm = this;
 
@@ -15,17 +15,24 @@
       partners: [],
       initialPartner: null,
       selectedPartner: null,
+      isInCancelProcess: false,
+      partnerWasChanged: false,
+      addressWasChanged: false,
 
       address: '',
 
-      selectPartner
+      selectPartner,
+      outletDataWasChanged,
+      submit,
+      cancel,
+      cancelConfirm
 
     });
 
     var Outlet = Schema.model('Outlet');
     var Partner = Schema.model('Partner');
 
-    vm.busy = findOutlet();
+    findOutlet();
 
     //$scope.$watch('vm.partner', (newValue) => {
     //  console.log('vm.partner', newValue);
@@ -37,7 +44,9 @@
 
     function findOutlet() {
 
-      return Outlet.find($state.params.id)
+      vm.busyMessage = 'Загрузка точки…';
+
+      vm.busy = Outlet.find($state.params.id)
         .then((outlet) => {
 
           vm.outlet = outlet;
@@ -59,6 +68,78 @@
 
     function selectPartner(partner) {
       vm.selectedPartner = partner;
+    }
+
+    function outletDataWasChanged() {
+
+      vm.partnerWasChanged = vm.initialPartner !== vm.selectedPartner;
+      vm.addressWasChanged = vm.outlet && vm.outlet.address !== vm.address;
+
+      return vm.partnerWasChanged || vm.addressWasChanged;
+
+    }
+
+    function saveNewData() {
+
+      if (outletDataWasChanged()) {
+
+        if (vm.partnerWasChanged) {
+          vm.outlet.partnerId = vm.selectedPartner.id;
+        }
+
+        if (vm.addressWasChanged) {
+          vm.outlet.address = vm.address;
+        }
+
+        return saveOutlet();
+
+      }
+
+    }
+
+    function saveOutlet() {
+
+      vm.busyMessage = 'Сохранение точки…';
+
+      vm.busy = Outlet.save(vm.outlet)
+        .then(quit)
+        .catch((err) => showSaveErrorAlert(err));
+
+    }
+
+    function showSaveErrorAlert(err) {
+
+      return ConfirmModal.show({
+        text: err,
+        question: 'Повторить попытку'
+      })
+        .then(saveOutlet);
+
+    }
+
+    function submit() {
+
+      _.result($window.document, 'activeElement.blur');
+      return saveNewData();
+
+    }
+
+    function cancel(form) {
+
+      if (form.$pristine) {
+        quit();
+      } else {
+        vm.isInCancelProcess = true;
+      }
+
+    }
+
+    function cancelConfirm() {
+      quit();
+    }
+
+    function quit() {
+      return $state.go('^');
     }
 
   }
