cd01657874f70aeaaa661ebf9ba9ff2c6ea428fe
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 15:33:14 +0300

    show outlets with visits info in partner.jade

--- src/app/domain/sales/ctrl/Partner.controller.js
+++ src/app/domain/sales/ctrl/Partner.controller.js
@@ -2,12 +2,48 @@
 
 (function () {
 
-  function PartnerController() {
+  function PartnerController(Schema, $state) {
 
     var vm = this;
 
     _.assign(vm, {
+
+      partner: null,
+      outlets: []
+
     });
+
+    var Partner = Schema.model('Partner');
+    var Outlet = Schema.model('Outlet');
+
+    findPartner();
+
+    function findPartner() {
+
+      vm.busyMessage = 'Загрузка контрагента…';
+
+      vm.busy = Partner.find($state.params.id)
+        .then((partner) => {
+
+          vm.partner = partner;
+
+          return Outlet.findAllWithRelations({partnerId: vm.partner.id})('Visit')
+            .then((outlets) => {
+
+              vm.outlets = outlets;
+              _.each(vm.outlets, (o) => {
+
+                var lastVisit = _.last(_.sortBy(o.visits, 'deviceCts'));
+                o.lastVisitDate = lastVisit ? lastVisit.deviceCts : '';
+
+              });
+
+            });
+
+        });
+
+    }
+
 
   }
 
