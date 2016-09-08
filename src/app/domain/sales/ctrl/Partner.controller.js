ae124c9d74fb7068a7645381f2b4b499452e7591
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 16:57:43 +0300

    add outletClick function to Partner.controller.js

--- src/app/domain/sales/ctrl/Partner.controller.js
+++ src/app/domain/sales/ctrl/Partner.controller.js
@@ -14,6 +14,7 @@
       deletePartnerClick,
       editPartnerClick,
       toggleOutletsSection,
+      outletClick,
       newOutletClick
 
     });
@@ -64,6 +65,10 @@
 
     function toggleOutletsSection() {
       vm.collapseOutletsSection = !vm.collapseOutletsSection;
+    }
+
+    function outletClick(outlet) {
+      $state.go('^.outlet', {id: outlet.id});
     }
 
     function newOutletClick() {
