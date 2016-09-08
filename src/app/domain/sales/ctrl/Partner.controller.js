a889aa7969fcfc15f74a1e65c777636c93ecdb25
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-09-08 11:47:06 +0300

    add Partner.controller.js

---
+++ src/app/domain/sales/ctrl/Partner.controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  function PartnerController() {
+
+    var vm = this;
+
+    _.assign(vm, {
+    });
+
+  }
+
+  angular.module('webPage')
+    .controller('PartnerController', PartnerController)
+  ;
+
+}());
