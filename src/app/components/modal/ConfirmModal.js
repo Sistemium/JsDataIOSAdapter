94beea1587a9ca50e494cb564a1ec8ee7169447e
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-10-10 17:06:37 +0300

    refactor ConfirmModal.js

--- src/app/components/modal/ConfirmModal.js
+++ src/app/components/modal/ConfirmModal.js
@@ -6,15 +6,22 @@
 
     function showErrorAskRepeat(onSuccess, onError) {
       return (err) => {
-        return show({
-          text: err.text || err,
-          question: 'Повторить попытку',
-          textClass: 'text-danger',
-          title: 'Ошибка!'
-        },{
-          windowClass: 'modal-warning'
-        }).then(onSuccess, onError);
+        return showMessageAskRepeat(err, onSuccess, onError);
       };
+    }
+
+    function showMessageAskRepeat(msg, onSuccess, onError) {
+
+      return show({
+        text: msg.text || msg,
+        question: 'Повторить попытку',
+        textClass: 'text-danger',
+        title: 'Ошибка!'
+      },{
+        windowClass: 'modal-warning'
+      })
+        .then(onSuccess, onError);
+
     }
 
     function show(config, modalConfig) {
@@ -33,15 +40,6 @@
 
             title: 'Внимание!',
             text: 'Вы действительно хотите сделать это?',
-
-            submit: function (buttonId) {
-              $uibModalInstance.close(buttonId);
-            },
-
-            cancel: function (buttonId) {
-              $uibModalInstance.dismiss(buttonId);
-            },
-
             buttons: [
               {
                 title: 'Да',
@@ -54,10 +52,13 @@
                 type: 'cancel'
               }
             ],
-
             hideCloseButton: false,
 
-            buttonClick: function (buttonId, buttonType) {
+            submit: (buttonId) => $uibModalInstance.close(buttonId),
+            cancel: (buttonId) => $uibModalInstance.dismiss(buttonId),
+            deleteItem: () => me.busy = me.deleteDelegate().then(me.cancel),
+
+            buttonClick: (buttonId, buttonType) => {
               switch (buttonType) {
                 case 'submit':
                 {
@@ -70,11 +71,6 @@
                   break;
                 }
               }
-            },
-
-            deleteItem: function () {
-              me.busy = me.deleteDelegate()
-                .then(me.cancel);
             }
 
           }, config));
@@ -99,7 +95,8 @@
 
     return {
       show,
-      showErrorAskRepeat
+      showErrorAskRepeat,
+      showMessageAskRepeat
     };
 
   }
