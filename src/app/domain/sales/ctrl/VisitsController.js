8c59f226de50f4dde30af87639f928b171c2b2f2
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-11-09 12:33:49 +0300

    add visits.jade and VisitsController.js

---
+++ src/app/domain/sales/ctrl/VisitsController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+    function VisitsController() {
+
+        var vm = this;
+
+        _.assign(vm, {
+            
+        });
+
+    }
+
+    angular.module('webPage')
+        .controller('VisitsController', VisitsController)
+    ;
+
+}());
