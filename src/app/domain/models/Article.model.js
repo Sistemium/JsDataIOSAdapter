bc0bc1d4c520e6797b7badfc7b6bc128858bf4fb
TaWReOn <deniss.mosin@gmail.com>
2016-11-29 15:46:25 +0200

    Article model has many relation

--- src/app/domain/models/Article.model.js
+++ src/app/domain/models/Article.model.js
@@ -2,81 +2,85 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema) {
+  angular.module('Models').run(function (Schema) {
 
-      Schema.register ({
+    Schema.register({
 
-        name: 'Article',
+      name: 'Article',
 
-        relations: {
-          belongsTo: {
-            ArticleGroup: {
-              localField: 'ArticleGroup',
-              localKey: 'articleGroup'
-            }
-          },
-          hasMany: {
-            StockBatch: {
-              localField: 'stockBatches',
-              foreignKey: 'article'
-            }
+      relations: {
+        belongsTo: {
+          ArticleGroup: {
+            localField: 'ArticleGroup',
+            localKey: 'articleGroup'
           }
         },
-
-        computed: {
-          firstName: ['name', function (name) {
-            var m = name.match(/"[^"]+"/);
-            return (m && m.length) ? m[0] : null;
-          }],
-          category: ['name', function (name) {
-            var m = name.match(/^[^ ]*/);
-            return (m && m.length) ? m[0].replace (/[^а-яa-z]/ig,' ') : null;
-          }],
-          factory: ['name', function (name) {
-            var m = name.match(/[ ][^ ]+[ ]/);
-            return (m && m.length) ? m[0].replace (/[^а-яa-z]/ig,' ') : null;
-          }],
-          lastName: ['name', function (name) {
-            var m = name.match(/"[^"]+" ([^,]*)/);
-            m = (m && m.length > 1) ? m[1] : '';
-            return m.replace (/\(.*[xх]+[ ]*[0-9]+[ ]*\)/,'') ;
-          }],
-          sameId: ['articleSame', 'id', function (articleSame, id) {
-            return articleSame || id;
-          }]
-        },
-
-        fieldTypes: {
-          packageRel: 'int',
-          pieceVolume: 'decimal'
-        },
-
-        methods: {
-
-          boxVolume: function (volume) {
-            return volume / this.packageRel;
+        hasMany: {
+          StockBatch: {
+            localField: 'stockBatches',
+            foreignKey: 'article'
           },
+          SaleOrderPosition: {
+            localField: 'saleOrders',
+            foreignKey: 'articleId'
+          }
+        }
+      },
 
-          boxPcs: function (volume) {
+      computed: {
+        firstName: ['name', function (name) {
+          var m = name.match(/"[^"]+"/);
+          return (m && m.length) ? m[0] : null;
+        }],
+        category: ['name', function (name) {
+          var m = name.match(/^[^ ]*/);
+          return (m && m.length) ? m[0].replace(/[^а-яa-z]/ig, ' ') : null;
+        }],
+        factory: ['name', function (name) {
+          var m = name.match(/[ ][^ ]+[ ]/);
+          return (m && m.length) ? m[0].replace(/[^а-яa-z]/ig, ' ') : null;
+        }],
+        lastName: ['name', function (name) {
+          var m = name.match(/"[^"]+" ([^,]*)/);
+          m = (m && m.length > 1) ? m[1] : '';
+          return m.replace(/\(.*[xх]+[ ]*[0-9]+[ ]*\)/, '');
+        }],
+        sameId: ['articleSame', 'id', function (articleSame, id) {
+          return articleSame || id;
+        }]
+      },
 
-            var rel = this.packageRel;
-            var box = Math.floor (volume / rel) || 0;
-            var pcs = volume - box * rel;
+      fieldTypes: {
+        packageRel: 'int',
+        pieceVolume: 'decimal'
+      },
 
-            return {
-              box: box,
-              pcs: pcs,
-              full: (box ? box + ' к' : '')
-              + (box && pcs && ' ' || '')
-              + (pcs ? pcs + ' б' : '')
-            }
+      methods: {
 
+        boxVolume: function (volume) {
+          return volume / this.packageRel;
+        },
+
+        boxPcs: function (volume) {
+
+          var rel = this.packageRel;
+          var box = Math.floor(volume / rel) || 0;
+          var pcs = volume - box * rel;
+
+          return {
+            box: box,
+            pcs: pcs,
+            full: (box ? box + ' к' : '')
+            + (box && pcs && ' ' || '')
+            + (pcs ? pcs + ' б' : '')
           }
 
         }
 
-      });
+      }
 
     });
+
+  });
 
 })();
