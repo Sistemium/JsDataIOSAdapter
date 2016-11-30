744ff600154c3e4e664f195385e959d935acb6ae
Alexander Levin <alevin.ru@gmail.com>
2016-11-30 23:56:41 +0200

    resize.directive attributed from vseramki

--- src/app/components/resize.directive.js
+++ src/app/components/resize.directive.js
@@ -2,22 +2,41 @@
 
 (function () {
 
-  function resize($window, $uibPosition) {
+  function resize($window, $uibPosition, $timeout) {
 
-    return (scope, element) => {
+    return (scope, element, attrs) => {
 
-      function getWindowDimensions () {
+      var property = attrs.resize ? (scope[attrs.resize] = {}) : scope;
+
+      function getWindowDimensions() {
+        var offset = $uibPosition.offset(element);
         return {
           windowHeight: $window.innerHeight,
           windowWidth: $window.innerWidth,
-          offsetTop: $uibPosition.offset(element).top
+          offsetTop: offset ? offset.top : 0
         };
       }
 
-      scope.$watch(getWindowDimensions, newValue => _.assign(scope,newValue), true);
+      function setValues(newValue) {
+        _.assign(property, newValue);
+      }
 
-      angular.element($window).bind('resize', () => scope.$apply());
+      var un = scope.$watch(getWindowDimensions, setValues, true);
 
+      function apply() {
+        scope.$apply();
+      }
+
+      angular.element($window)
+        .bind('resize', apply);
+
+      scope.$on('$destroy', ()=> {
+        un();
+        angular.element($window)
+          .unbind('resize', apply);
+      });
+
+      $timeout(setValues);
     }
 
   }
