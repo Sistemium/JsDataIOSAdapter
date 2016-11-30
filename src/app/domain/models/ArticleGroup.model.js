0a4cd345e14e8600d14635dad0332b66da16fb63
Alexander Levin <alevin.ru@gmail.com>
2016-11-30 23:37:43 +0200

    ArticleGroup with methods

--- src/app/domain/models/ArticleGroup.model.js
+++ src/app/domain/models/ArticleGroup.model.js
@@ -2,29 +2,61 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema) {
+  angular.module('Models').run(function (Schema) {
 
-      Schema.register({
+    let stockCache = {};
 
-        name: 'ArticleGroup',
+    Schema.register({
 
-        relations: {
-          hasMany: {
-            Article: {
-              localField: 'Articles',
-              foreignKey: 'articleGroup'
-            }
+      name: 'ArticleGroup',
+
+      relations: {
+        hasMany: {
+          Article: {
+            localField: 'Articles',
+            foreignKey: 'articleGroup'
           },
-          hasOne: {
-            ArticleGroup: {
-              localField: 'articleGroup',
-              localKey: 'articleGroupId'
-            }
+          ArticleGroup: {
+            localField: 'children',
+            foreignKey: 'articleGroupId'
+          }
+        },
+        hasOne: {
+          ArticleGroup: {
+            localField: 'articleGroup',
+            localKey: 'articleGroupId'
           }
         }
+      },
 
-      });
+      meta: {
+        setStock: function () {
+          stockCache = _.groupBy(Schema.model('Stock').getAll(), 'article.articleGroup');
+        }
+      },
+
+      methods: {
+        ancestors: function () {
+          let res = [];
+
+          if (this.articleGroupId) {
+            res.push(this.articleGroup);
+            let parents = this.articleGroup.ancestors();
+            if (parents.length) {
+              Array.prototype.push.apply(res, parents);
+            }
+          }
+
+          return res;
+
+        },
+        stockArticles: function () {
+          return _.get(stockCache[this.id], 'length');
+        }
+      }
 
     });
+
+  });
 
 })();
