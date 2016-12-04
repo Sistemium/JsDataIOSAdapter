65690ca84544c4f76d318ee71499845e893bcb15
Alexander Levin <alevin.ru@gmail.com>
2016-12-04 15:16:33 +0200

    salesmansMenu with noneClick and isEnabled

--- src/app/components/navbar/salesmansMenu.controller.js
+++ src/app/components/navbar/salesmansMenu.controller.js
@@ -7,12 +7,14 @@
     let vm = saControllerHelper.setup(this, $scope);
 
     vm.use({
+      isEnabled: false,
       salesmanClick: SalesmanAuth.login,
+      noneClick: SalesmanAuth.logout,
       onStateChange
     });
 
     SalesmanAuth
-      .bindAll($scope, 'vm.salesmans')
+      .bindAll($scope, 'vm.salesmans', onDataChange)
       .watchCurrent($scope, salesman => vm.selectedSalesman = salesman);
 
     /*
@@ -21,6 +23,10 @@
 
     function onStateChange(state) {
       vm.isSalesState = _.startsWith(state.name, 'sales.');
+    }
+
+    function onDataChange(e, data) {
+      vm.isEnabled = _.get(data, 'length') > 1;
     }
 
   }
