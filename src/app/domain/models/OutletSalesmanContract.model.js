4b8d2fc9c62e7db708ef5c46e77649c56fb44d53
Alexander Levin <alevin.ru@gmail.com>
2016-12-05 11:26:04 +0200

    OutletSalesmanContract.model

---
+++ src/app/domain/models/OutletSalesmanContract.model.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+
+  angular.module('Models').run(function (Schema) {
+
+    Schema.register({
+
+      name: 'OutletSalesmanContract',
+
+      relations: {
+        hasOne: {
+          Outlet: {
+            localField: 'outlet',
+            localKey: 'outletId'
+          },
+          Salesman: {
+            localField: 'salesman',
+            localKey: 'salesmanId'
+          }
+        }
+      }
+    });
+  });
+
+})();
