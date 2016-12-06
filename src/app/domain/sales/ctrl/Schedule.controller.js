5838d5b823172191510a84b1bf9a83db2e34d1d3
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-12-06 16:06:29 +0300

    vm.currentWeek

--- src/app/domain/sales/ctrl/Schedule.controller.js
+++ src/app/domain/sales/ctrl/Schedule.controller.js
@@ -2,8 +2,56 @@
 
 (function () {
 
-  function ScheduleController() {
+  function ScheduleController(saControllerHelper, $scope, SalesmanAuth) {
 
+    let vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+
+    });
+
+    /*
+     Listeners
+     */
+
+    SalesmanAuth.watchCurrent($scope, salesman => {
+
+      vm.selectedSalesmanId = _.get(salesman, 'id');
+      setupNewDay();
+
+    });
+
+    $scope.$watch(
+      () => new Date().setHours(0, 0, 0, 0),
+      (todayTime, oldValue) => {
+        (todayTime != oldValue) && setupNewDay(todayTime);
+      }
+    );
+
+    /*
+     Functions
+     */
+
+    function setupNewDay(todayTime) {
+
+      if (_.isUndefined(todayTime)) todayTime = new Date().setHours(0, 0, 0, 0);
+
+      vm.todayDate = new Date(todayTime);
+      console.log('new day started', vm.todayDate);
+
+      vm.currentWeek = [vm.todayDate];
+
+      for (var i = 1; i < 7; i++) {
+
+        var day = _.clone(vm.todayDate);
+        day.setDate(day.getDate() + i);
+        vm.currentWeek.push(new Date(day));
+
+      }
+
+      console.log(vm.currentWeek);
+
+    }
   }
 
   angular.module('webPage')
