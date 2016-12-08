13e17af573f5028bab8cc0bbaa990c7fb7d5f6a0
TaWReOn <deniss.mosin@gmail.com>
2016-12-08 18:44:25 +0200

    collapsible articleGroups in catalogue

--- src/app/domain/sales/ctrl/Catalogue.controller.js
+++ src/app/domain/sales/ctrl/Catalogue.controller.js
@@ -23,6 +23,7 @@
       search: $state.params.q || '',
       saleOrderId: $state.params.saleOrderId,
       isOpenOutletPopover: false,
+      isWideScreen: isWideScreen(),
 
       articleGroupClick: setCurrentArticleGroup,
       priceTypeClick,
@@ -75,6 +76,12 @@
       (newValue, oldValue) => newValue != oldValue && $scope.$broadcast('vsRepeatTrigger')
     );
 
+    $scope.$watch(
+      isWideScreen,
+      (newValue, oldValue) => vm.isWideScreen = newValue
+    );
+
+
     /*
      Handlers
      */
@@ -126,6 +133,10 @@
     /*
      Functions
      */
+
+    function isWideScreen() {
+      return !saMedia.xsWidth && !saMedia.xxsWidth;
+    }
 
     function articleRowHeight(stock) {
 
@@ -257,6 +268,8 @@
         vm.articleGroups = children;
 
       } else if (articleGroup && articleGroup.articleGroup) {
+
+        vm.isArticleGroupsExpanded = false;
 
         ownStock = getStockByArticlesOfGroup(articleGroup.articleGroup);
         groupIds = articleGroupIds(ownStock);
