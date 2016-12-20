e1eb7a3a40cfea845c68108cf05688a5340f1d25
Alexander Levin <alevin.ru@gmail.com>
2016-12-20 11:19:20 +0200

    custom datePickerPopup template

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -16,6 +16,10 @@
       templateUrl: 'app/components/busy/busy.html'
     })
 
+    .config(uibDatepickerPopupConfig => {
+      uibDatepickerPopupConfig.datepickerPopupTemplateUrl = 'app/domain/components/datePicker/datePickerPopup.html';
+    })
+
     .run(moment => {
       moment.defaultFormat = 'YYYY-MM-DD';
     })
