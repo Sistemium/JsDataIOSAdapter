9c57a9f1d5785b9a9f5ae617368c94f61f75e326
Alexander Levin <alevin.ru@gmail.com>
2017-01-03 16:30:55 +0300

    CatalogueAlert.model

---
+++ src/app/domain/models/CatalogueAlert.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+
+  angular.module('Models').run(function (Schema, toastr) {
+
+    const defaultOptions = {timeOut: 10000};
+
+    const CatalogueAlert = Schema.register({
+
+      name: 'CatalogueAlert',
+
+      relations: {
+        hasOne: {
+          ArticleGroup: {
+            localField: 'articleGroup',
+            localKey: 'articleGroupId'
+          }
+        }
+      },
+
+      meta: {},
+
+      methods: {
+
+        show: function () {
+          this.lastShown = moment();
+          return toastr[this.style || 'warning'](this.text, this.title, this.options || defaultOptions);
+        }
+
+      }
+
+    });
+
+    _.map([
+    ], i => CatalogueAlert.inject(i));
+
+
+  });
+
+})();
