9ac99724492dc429495af0c38a81e0a3952f65cf
Alexander Levin <alevin.ru@gmail.com>
2017-03-11 10:43:29 +0200

    refactor ngGallery with controller

--- src/app/components/gallery/ngGallery.js
+++ src/app/components/gallery/ngGallery.js
@@ -5,9 +5,12 @@
   angular.module('webPage')
     .directive('ngGallery', ngGallery);
 
-  function GalleryController($scope) {
+  function GalleryController($scope, $q, $templateRequest, $compile, $timeout, $document, $window) {
 
     const vm = this;
+    const $body = $document.find('body');
+
+    let fullscreenElement;
 
     _.assign(vm, {
 
@@ -17,31 +20,194 @@
 
         $scope.index = $scope.imagesAll.indexOf(img);
 
-        const fn = $scope.thumbnailClickFn() || $scope.openGallery;
+        const fn = $scope.thumbnailClickFn() || openGallery;
 
         return _.isFunction(fn) && fn(img);
 
-      }
+      },
+
+      fullScreenThumbnailClick: function (img) {
+        $scope.index = $scope.imagesAll.indexOf(img);
+        showImage(img);
+      },
+
+      nextImageClick: setNextImage,
+      prevImageClick: setPrevImage,
+
+      closeGalleryClick: cleanup,
+
+      deleteClick
 
     });
 
-  }
+    $scope.$on('$destroy', cleanup);
 
-  function ngGallery($document, $timeout, $q, $templateRequest, $compile) {
+    /*
+     Functions
+     */
 
-    var defaults = {
-      baseClass: 'ng-gallery',
-      thumbClass: 'ng-thumb',
-      templateUrl: 'app/components/gallery/galleryTemplate.html'
-    };
 
-    var keys_codes = {
+    function deleteCurrentImage() {
+
+      let imageModel = $scope.imagesAll[$scope.index];
+
+      if (!imageModel) {
+        return console.error('ngGallery.deleteCurrentImage Failed to get image model');
+      }
+
+      imageModel.DSDestroy()
+        .then(cleanup)
+        .catch(cleanup);
+
+    }
+
+    function deleteClick() {
+      if ($scope.confirmDelete) deleteCurrentImage();
+      $scope.confirmDelete = !$scope.confirmDelete;
+    }
+
+    function cleanup() {
+      $scope.opened = false;
+      $body.unbind('keydown', onKeyDown);
+      fullscreenElement && fullscreenElement.remove();
+    }
+
+    function setNextImage() {
+      setImageByIndex($scope.index + 1);
+    }
+
+    function setPrevImage() {
+      setImageByIndex($scope.index - 1);
+    }
+
+    function setImageByIndex(index) {
+      let images = $scope.imagesAll;
+      index = (images.length + index) % images.length;
+      showImage(images[$scope.index = index]);
+    }
+
+    function openGallery(image) {
+
+      $templateRequest('app/components/gallery/galleryFullscreen.html')
+        .then(function (html) {
+          let template = angular.element(html);
+          $body.append(template);
+          fullscreenElement = $compile(template)($scope);
+          $body.bind('keydown', onKeyDown);
+          scrollThumbnailsToCurrent();
+        });
+
+      $scope.opened = true;
+
+      return angular.isDefined(image) && showImage(image);
+
+    }
+
+    function loadImage(img) {
+
+      return $q((resolve, reject) => {
+
+        const image = _.assign(new Image(), {
+
+          onload: function () {
+            if (this.complete === false || this.naturalWidth === 0) {
+              reject();
+            }
+            resolve(image);
+          },
+
+          onerror: reject,
+          src: img.smallSrc
+
+        });
+
+      });
+
+    }
+
+    function showImage(img) {
+
+      $scope.loading = true;
+      $scope.confirmDelete = false;
+
+      loadImage(img)
+        .then(function (res) {
+          $scope.currentImageSrc = res.src;
+          // scope.id = img.name;
+          // smartScroll(scope.index);
+        })
+        .catch(() => {
+          $scope.imageLoadingError = true;
+        })
+        .finally(() => {
+          $scope.loading = false;
+        });
+
+    }
+
+    const keyCodes = {
       enter: 13,
       esc: 27,
       left: 37,
       right: 39
     };
 
+    function onKeyDown(event) {
+
+      if (!$scope.opened) {
+        return console.error('!scope.opened');
+      }
+
+      let which = event.which;
+
+      if (which === keyCodes.esc) {
+        cleanup();
+      } else if (which === keyCodes.right || which === keyCodes.enter) {
+        setNextImage();
+      } else if (which === keyCodes.left) {
+        setPrevImage();
+      }
+
+      $scope.$apply();
+
+    }
+
+    function scrollThumbnailsToCurrent() {
+
+      $timeout(function () {
+
+        let index = $scope.index;
+
+        let thumbWrapper = $window.document.getElementById('ng-thumbnails-scroll');
+
+        // TODO: Refactor if statement
+
+        if (index != 0 && (($scope.imagesAll.length - 7) != index) && $scope.firstOpen == false) {
+          thumbWrapper.scrollLeft = 68 * index + 2;
+        } else {
+          if (($scope.imagesAll.length - 7) == index) {
+            thumbWrapper.scrollLeft = 68 * index + 4;
+          } else {
+            thumbWrapper.scrollLeft = 68 * index;
+          }
+
+        }
+
+        $scope.firstOpen = false;
+
+      }, 0);
+
+    }
+
+  }
+
+  function ngGallery($timeout) {
+
+    const defaults = {
+      baseClass: 'ng-gallery',
+      thumbClass: 'ng-thumb',
+      templateUrl: 'app/components/gallery/galleryTemplate.html'
+    };
 
     function setScopeValues(scope) {
       scope.baseClass = scope.class || defaults.baseClass;
@@ -78,192 +244,24 @@
           scope.thumbsNum = 11;
         }
 
-        function hideElement() {
+        function hideNavElements() {
           $timeout(function () {
             scope.showNavElems = false;
           }, 2000);
         }
 
-        hideElement();
-
-        var $body = $document.find('body');
+        hideNavElements();
 
         scope.showNavElems = true;
         scope.index = 0;
         scope.opened = false;
         scope.firstOpen = true;
 
-        function loadImage(img) {
-
-          return $q((resolve, reject) => {
-
-            const image = new Image();
-
-            scope.loading = true;
-
-            image.onload = function () {
-              scope.loading = false;
-              if (this.complete === false || this.naturalWidth === 0) {
-                reject();
-              }
-              resolve(image);
-            };
-
-            image.onerror = function (err) {
-              reject(err);
-            };
-
-            image.src = img.smallSrc;
-
-          });
-
-        }
-
-        function showImage(img) {
-
-          loadImage(img)
-            .then(function (res) {
-              scope.currentImageSrc = res.src;
-              // scope.id = img.name;
-              // smartScroll(scope.index);
-            });
-
-          scope.confirmDelete = false;
-
-        }
-
-        var fullscreenElement;
-
-        //var defineClass = function (width, height) {
-        //  scope.useWide = false, scope.useTall = false;
-        //  width >= height ? scope.useWide = true : scope.useTall = true;
-        //};
-
-        scope.changeImage = function (img, index) {
-          scope.index = index;
-          showImage(img);
-        };
-
-        scope.nextImage = function () {
-          setImageByIndex(scope.index + 1);
-        };
-
-
-        scope.prevImage = function () {
-          setImageByIndex(scope.index - 1);
-        };
-
-        function setImageByIndex(index) {
-          showImage(scope.imagesAll[scope.index = (scope.imagesAll.length + index) % scope.imagesAll.length]);
-        }
-
         scope.setHovered = function (image) {
           if (_.isFunction(scope.imageHoveredFn())) {
             scope.imageHoveredFn()(image);
           }
         };
-
-        scope.openGallery = function (image) {
-
-          $templateRequest('app/components/gallery/galleryFullscreen.html')
-            .then(function (html) {
-              let template = angular.element(html);
-              $body.append(template);
-              fullscreenElement = $compile(template)(scope);
-              $body.bind('keydown', onKeyDown);
-            });
-
-          scope.opened = true;
-
-          $timeout(smartScroll);
-
-          return angular.isDefined(image) && showImage(image);
-
-        };
-
-        function cleanup() {
-          scope.opened = false;
-          $body.unbind('keydown', onKeyDown);
-          fullscreenElement && fullscreenElement.remove();
-        }
-
-        scope.$on('$destroy', cleanup);
-
-        scope.closeGallery = cleanup;
-
-        scope.deletePhoto = function () {
-
-          var imageModel = scope.imagesAll[scope.index];
-
-          if (imageModel) {
-            imageModel.DSDestroy()
-              .then(() => {
-                scope.closeGallery();
-              })
-              .catch(err => {
-                scope.closeGallery();
-                //ToastHelper.error('Не удалось удалить изображение');
-                console.error(err);
-              });
-          } else {
-            console.error('ngGallery: Failed to initialize image model');
-          }
-
-        };
-
-        scope.deleteClick = function () {
-          if (scope.confirmDelete) {
-            scope.deletePhoto();
-          }
-          scope.confirmDelete = !scope.confirmDelete;
-        };
-
-        function onKeyDown (event) {
-
-          if (!scope.opened) {
-            return console.error('!scope.opened');
-          }
-
-          let which = event.which;
-
-          if (which === keys_codes.esc) {
-            scope.closeGallery();
-          } else if (which === keys_codes.right || which === keys_codes.enter) {
-            scope.nextImage();
-          } else if (which === keys_codes.left) {
-            scope.prevImage();
-          }
-
-          scope.$apply();
-
-        }
-
-        function smartScroll () {
-
-          $timeout(function () {
-
-            let index = scope.index;
-
-            let thumbWrapper = document.querySelectorAll('.ng-thumbnails');
-
-            // TODO: Refactor if statement
-
-            if (index != 0 && ((scope.imagesAll.length - 7) != index) && scope.firstOpen == false) {
-              thumbWrapper[0].scrollLeft = 68 * index + 2;
-            } else {
-              if ((scope.imagesAll.length - 7) == index) {
-                thumbWrapper[0].scrollLeft = 68 * index + 4;
-              } else {
-                thumbWrapper[0].scrollLeft = 68 * index;
-              }
-
-            }
-
-            scope.firstOpen = false;
-
-          }, 0);
-
-        }
 
       }
     };
