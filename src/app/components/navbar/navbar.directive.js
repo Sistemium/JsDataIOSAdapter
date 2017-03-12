84e5192bdbeddf4555c6a204ef40335590fd21b0
Alexander Levin <alevin.ru@gmail.com>
2017-03-12 17:45:21 +0200

    mv navbar.controller.js into navbar.directive.js

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -1,23 +1,55 @@
 (function() {
-  'use strict';
 
-  angular
-    .module('webPage')
-    .directive('navbar', acmeNavbar);
-
-  /** @ngInject */
   function acmeNavbar() {
     return {
 
       restrict: 'E',
       templateUrl: 'app/components/navbar/root-navbar.html',
-      scope: {
-      },
-      controller: 'NavbarController',
+
+      scope: {},
+
+      controller: NavbarController,
       controllerAs: 'vm',
       bindToController: true
 
     };
   }
+
+  function NavbarController(Auth, Menu, $scope, $rootScope, saControllerHelper, localStorageService) {
+
+    const DEFAULT_TITLE = 'Главное меню';
+    const vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+
+      auth: Auth,
+      menu: Menu.root(),
+      rootClick: () => $rootScope.$broadcast('rootClick'),
+      showImages: localStorageService.get('showImages') || false
+
+    });
+
+    $scope.$on('$stateChangeSuccess', onStateChange);
+    $scope.$on('$stateChangeStart', onStateChange);
+
+    function onStateChange(event, to) {
+
+      vm.use({
+        hide: !!_.get(to, 'data.hideTopBar'),
+        hideNavs: !!_.get(to, 'data.hideNavs'),
+        title: _.get(to, 'data.title') || DEFAULT_TITLE,
+        isRootState: to.name === 'home',
+        currentItem: _.find(vm.menu.items, item => to.name && _.startsWith(to.name, item.state)),
+        isSalesState: _.startsWith(to.name, 'sales.'),
+        isCatalogueState: _.startsWith(to.name, 'sales.catalogue')
+      });
+
+    }
+
+  }
+
+  angular
+    .module('webPage')
+    .directive('navbar', acmeNavbar);
 
 })();
