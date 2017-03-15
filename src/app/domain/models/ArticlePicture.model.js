ac9826d7c3ddf49f92001ec71d3a3d0fba2aa3f8
gogbajbobo <grigoriev.maxim@gmail.com>
2017-03-15 11:53:23 +0300

    add srcThumbnail and srcFullscreen computed property

--- src/app/domain/models/ArticlePicture.model.js
+++ src/app/domain/models/ArticlePicture.model.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  angular.module('Models').run(function (Schema) {
+  angular.module('Models').run((Schema, $window) => {
 
     Schema.register({
 
@@ -23,9 +23,44 @@
             foreignKey: 'avatarPictureId'
           }
         }
+      },
+
+      computed: {
+
+        srcThumbnail  : ['thumbnailSrc', 'thumbnailPath', (imageSrc, imagePath) => {
+          return actingImageSrc(imageSrc, imagePath);
+        }],
+        srcFullscreen : ['smallSrc', 'resizedImagePath', (imageSrc, imagePath) => {
+          return actingImageSrc(imageSrc, imagePath);
+        }]
+
       }
 
     });
+
+    function actingImageSrc(imageSrc, imagePath) {
+
+      if (_.isString(imageSrc)) {
+
+        return imageSrc;
+
+      } else  {
+
+        switch ($window.location.protocol) {
+          case 'http:'  :
+          case 'https:' : return () => {
+
+            // we are in iOS but not under manifest, pechalka
+            console.log('we are in iOS but not under manifest, pechalka');
+            return imageSrc;
+
+          };
+          case 'file:'  : return '../../../../' + imagePath;
+        }
+
+      }
+
+    }
 
   });
 
