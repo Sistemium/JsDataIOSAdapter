d5a20324f3dc64d3b3e9f08fdee293842794374b
Alexander Levin <alevin.ru@gmail.com>
2017-03-25 13:29:57 +0200

    Article.tags

--- src/app/domain/models/Article.model.js
+++ src/app/domain/models/Article.model.js
@@ -4,7 +4,18 @@
 
   angular.module('Models').run(function (Schema) {
 
-    const re = /защищ[^ .]*[ .]*наим[^ .]*[ .]*места проис[^ ]*|защ[^ .]*[ .]*геогр[^ .]*[ .]*указ[^ ]*|геогр[^ .]*[ .]*защ[^ ]* наим[^ ]*|геог[^ .]*[ .]*наим[^ ]*|защищ[^ .]*[ .]*наим[^ ]*/i;
+    const re = /защищ[^ .]*[ .]*наим[^ .]*[ .]*места проис[^ ]*|защ[^ .]*[ .]*геогр[^ .]*[ .]*указ[^ ]*|геогр[^ .]*[ .]*указ[^ ]*|геогр[^ .]*[ .]*защ[^ ]* наим[^ ]*|геог[^ .]*[ .]*наим[^ ]*|защищ[^ .]*[ .]*наим[^ ]*/i;
+
+    const tagRegs = {
+      red: ['красное', /красн\.|красное|кр\.(?![^\d][\d]+)/i],
+      dry: ['сухое', /сухое|сух\./i],
+      semiDry: ['п/сух', /п\/сух\./i],
+      sweet: ['сладкое', /сладкое|сл\./i],
+      semiSweet: ['п/сл', /п\/сл\./i],
+      brut: ['брют', /брют/i],
+      white: ['белое', /бел\.|белое/i],
+      gift: ['п/у', /подар[^ .]*|под[^ .]*[ .]{1,2}упа[^ .)]*|в п\/у[^ .)]*|п\/у[^ .)]*/i]
+    };
 
     Schema.register({
 
@@ -49,11 +60,20 @@
       notify: false,
 
       computed: {
+        tags: ['name', tagger],
         preName: ['name', function (name) {
+
           let m = name.match(/[^"]+/);
           let res = (m && m.length) ? _.trim(m[0]) : null;
+
           res = _.replace(res, re, 'ЗНМП');
-          return res;
+
+          _.each(tagRegs, (cfg) => {
+            res = _.replace(res, cfg[1], '');
+          });
+
+          return _.trim(res);
+
         }],
         firstName: ['name', function (name) {
           var m = name.match(/"[^"]+"/);
@@ -76,19 +96,24 @@
           m = _.trim(m.replace(/^,[^ ]*/, ''));
           let res = m.replace(/\(.*[xх]+[ ]*[0-9]+[ ]*\)/, '');
           res = _.replace(res, re, 'ЗНМП');
-          return res;
+
+          _.each(tagRegs, (cfg) => {
+            res = _.replace(res, cfg[1], '');
+          });
+
+          return _.trim(res);
         }],
         sameId: ['articleSame', 'id', function (articleSame, id) {
           return articleSame || id;
         }],
-        pcsLabel: ['pieceVolume', function(pieceVolume) {
+        pcsLabel: ['pieceVolume', function (pieceVolume) {
           return pieceVolume ? 'б' : 'шт';
         }]
       },
 
       // fieldTypes: {
-        // packageRel: 'int',
-        // pieceVolume: 'decimal'
+      // packageRel: 'int',
+      // pieceVolume: 'decimal'
       // },
 
       methods: {
@@ -102,12 +127,12 @@
           var rel = this.packageRel;
           var box = rel > 1 ? Math.floor(volume / rel) : 0;
           var pcs = volume - box * rel;
-          let half = pcs*2 === rel;
+          let half = pcs * 2 === rel;
 
           return {
             box: box,
             pcs: pcs,
-            full: (box || half ? `${box||''}${half&&'½'||''} к` : '')
+            full: (box || half ? `${box || ''}${half && '½' || ''} к` : '')
             + (box && pcs && !half && ' ' || '')
             + (pcs && !half ? `${pcs} ${this.pcsLabel}` : '')
           }
@@ -117,6 +142,21 @@
       }
 
     });
+
+    function tagger(name) {
+
+      let res = _.map(tagRegs, (cfg, code) => {
+        if (cfg[1].test(name)) {
+          return {
+            code: code,
+            label: cfg[0]
+          };
+        }
+      });
+
+      return _.filter(res);
+
+    }
 
   });
 
