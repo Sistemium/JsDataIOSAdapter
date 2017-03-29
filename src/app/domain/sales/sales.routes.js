9ff96aebbfb1c95759ef902c9823677a182721cc
gogbajbobo <grigoriev.maxim@gmail.com>
2017-03-29 18:33:19 +0300

    add campaignGroup to url parameters

--- src/app/domain/sales/sales.routes.js
+++ src/app/domain/sales/sales.routes.js
@@ -189,7 +189,7 @@
       var campaigns = {
 
         name: 'campaigns',
-        url: '/campaigns',
+        url: '/campaigns?campaignGroup',
 
         templateUrl: 'app/domain/sales/campaigns/campaigns.html',
         controller: 'CampaignsController',
