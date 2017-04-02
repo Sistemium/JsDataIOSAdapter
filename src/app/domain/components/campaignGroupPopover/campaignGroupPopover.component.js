b5a45dfdd06dbfc2a1f16d0fe4d40595d15fb6b2
Alexander Levin <alevin.ru@gmail.com>
2017-04-02 11:59:16 +0300

    campaignGroupPopover.component mv to Sales module

--- src/app/domain/components/campaignGroupPopover/campaignGroupPopover.component.js
+++ src/app/domain/components/campaignGroupPopover/campaignGroupPopover.component.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  angular.module('webPage').component('campaignGroupPopover', {
+  angular.module('Sales').component('campaignGroupPopover', {
 
     bindings: {
       currentItem: '=',
