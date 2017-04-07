72a898255968ebe7d2199f7186b02f8f9c6103c4
Alexander Levin <alevin.ru@gmail.com>
2017-04-07 14:47:32 +0300

    mapsHelper back in VisitCreateController to show map

--- src/app/domain/sales/ctrl/VisitCreateController.js
+++ src/app/domain/sales/ctrl/VisitCreateController.js
@@ -4,9 +4,10 @@
 
   const REQUIRED_ACCURACY = 150;
 
-  function VisitCreateController(Schema, $scope, $state, $q, SalesmanAuth, geolib, Helpers) {
+  function VisitCreateController(Schema, $scope, $state, $q, SalesmanAuth, geolib, Helpers, mapsHelper) {
 
     const {ConfirmModal, toastr, PhotoHelper, LocationHelper, saControllerHelper} = Helpers;
+    const {yLatLng, distanceFn} = mapsHelper;
 
     const {Visit, Location} = Schema.models();
     const VQS = Schema.model('VisitQuestionSet');
