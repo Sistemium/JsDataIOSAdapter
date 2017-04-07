98c0872e49b589e56e4927e2c15460c9b33b0235
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-07 15:07:04 +0300

    add photoReports.controller.js

---
+++ src/app/domain/sales/photoReports/photoReports.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+
+  function PhotoReportsController(/*Schema*/) {
+
+    // const {PhotoReport} = Schema.models();
+
+  }
+
+  angular.module('webPage')
+    .controller('PhotoReportsController', PhotoReportsController);
+
+}());
