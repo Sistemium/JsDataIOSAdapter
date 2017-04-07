42b8992f8730bc74e5eb7736cfb3cc62d88d9d5a
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-07 18:35:06 +0300

    show outlets in photoReports

--- src/app/domain/sales/photoReports/photoReports.controller.js
+++ src/app/domain/sales/photoReports/photoReports.controller.js
@@ -2,9 +2,55 @@
 
 (function () {
 
-  function PhotoReportsController(/*Schema*/) {
+  function PhotoReportsController(Schema, Helpers, $scope, SalesmanAuth/*, $state*/) {
 
-    // const {PhotoReport} = Schema.models();
+    const {Partner/*, PhotoReport, Outlet*/} = Schema.models();
+    const {saMedia, saControllerHelper} = Helpers;
+
+    const vm = saControllerHelper.setup(this, $scope)
+      .use({
+        takePhoto,
+        outletClick,
+        rowHeight
+      });
+
+    findOutlets();
+
+    function findOutlets() {
+
+      let filter = SalesmanAuth.makeFilter();
+      // let outletFilter = Outlet.meta.salesmanFilter(filter);
+
+      return Partner.findAllWithRelations(filter, {bypassCache: true})(['Outlet'])
+        .then(partners => {
+
+          console.info(partners);
+          vm.partners = partners;
+
+          console.info(_.first(partners).outlets);
+
+        });
+
+    }
+
+    function takePhoto() {
+
+      console.info('takePhoto()');
+      // $state.go('sales.territory');
+      // return PhotoHelper.takePhoto('PhotoReport', {visitId: vm.visit.id}, vm.thumbnails);
+
+    }
+
+    function outletClick(outlet) {
+      console.info('outletClick', outlet);
+    }
+
+    function rowHeight(partner) {
+
+      let xsMargin = (saMedia.xsWidth || saMedia.xxsWidth) ? 21 : 0;
+      return 39 + partner.outlets.length * 29 + 8 + 17 - xsMargin;
+
+    }
 
   }
 
