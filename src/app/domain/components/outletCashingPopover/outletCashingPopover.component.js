4c82e5f5a301eeb5a438d993da85e9e6300eb51e
Alexander Levin <alevin.ru@gmail.com>
2017-04-16 12:52:30 +0300

    outletCashingPopover component

---
+++ src/app/domain/components/outletCashingPopover/outletCashingPopover.component.js
@@ -0,0 +1,83 @@
+'use strict';
+
+(function (module) {
+
+  module.component('outletCashingPopover', {
+
+    bindings: {
+      outlet: '<'
+    },
+
+    transclude: true,
+
+    templateUrl: 'app/domain/components/outletCashingPopover/outletCashingPopover.html',
+
+    controller: outletCashingPopoverController,
+    controllerAs: 'vm'
+
+  });
+
+  function outletCashingPopoverController(Schema, $scope) {
+
+    let vm = this;
+
+    _.assign(vm, {
+
+      $onInit,
+      onSubmit,
+      triggerClick
+
+    });
+
+    const {Cashing} = Schema.models();
+
+    /*
+     Init
+     */
+
+    /*
+     Listeners
+     */
+
+    /*
+     Functions
+     */
+
+    function triggerClick() {
+
+      vm.isPopoverOpen = !vm.isPopoverOpen;
+
+    }
+
+    function onSubmit() {
+
+      let {summ, ndoc} = vm;
+
+      if (summ <= 0) return;
+
+      let cashing = Cashing.createInstance({
+        debtId: null,
+        summ,
+        ndoc,
+        outletId: vm.outlet.id,
+        date: moment().format(),
+        uncashingId: null
+      });
+
+      Cashing.create(cashing)
+        .then(saved => {
+          vm.isPopoverOpen = false;
+          $scope.$emit('DebtOrCashingModified');
+        });
+    }
+
+    function $onInit() {
+      $scope.$watch('vm.isPopoverOpen', () => {
+        vm.ndoc = null;
+        vm.summ = null;
+      });
+    }
+
+  }
+
+})(angular.module('Sales'));
