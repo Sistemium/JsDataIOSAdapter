df0c3e855ad77fa1d4bb98bac9128e489dde99b3
Alexander Levin <alevin.ru@gmail.com>
2017-04-25 18:14:05 +0300

    menu with badges

--- src/app/domain/menu/menu.service.js
+++ src/app/domain/menu/menu.service.js
@@ -7,81 +7,90 @@
     const SALES_ROLES = ['salesman', 'supervisor'];
     const PICKING_ROLES = ['picker'];
 
-    return {
-      root: function () {
-        let menu = {
+    const items = [{
+      title: 'Сборка',
+      state: 'picking.orderList({state:"notdone"})',
+      needRoles: PICKING_ROLES
+    }, {
+      title: 'Сборщик',
+      state: 'picker',
+      needRoles: PICKING_ROLES
+    }, {
+      title: 'Каталог',
+      state: 'sales.catalogue',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Заказы',
+      state: 'sales.saleOrders',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Отгрузки',
+      state: 'sales.shipmentList',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Долги',
+      state: 'sales.debtByOutlet',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Выручка',
+      state: 'sales.cashing',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Клиенты',
+      state: 'sales.territory',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Акции',
+      state: 'sales.campaigns',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Фотоотчёты',
+      state: 'sales.photoReports',
+      needRoles: ['admin', 'tester']
+      // needRoles: SALES_ROLES
+    }, {
+      title: 'Визиты',
+      state: 'sales.visits',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Фотопоток',
+      state: 'photos.stream',
+      needRoles: SALES_ROLES
+    }, {
+      title: 'Предзаказы',
+      state: 'sales.prePreOrders',
+      needRoles: 'pre-ordering'
+    }, {
+      title: 'Тесты',
+      state: 'playground',
+      needRoles: ['admin', 'tester']
+    }];
 
-          title: 'Начало',
-          state: 'home',
 
-          items: [{
-            title: 'Сборка',
-            state: 'picking.orderList({state:"notdone"})',
-            needRoles: PICKING_ROLES
-          }, {
-            title: 'Сборщик',
-            state: 'picker',
-            needRoles: PICKING_ROLES
-          }, {
-            title: 'Каталог',
-            state: 'sales.catalogue',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Заказы',
-            state: 'sales.saleOrders',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Отгрузки',
-            state: 'sales.shipmentList',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Долги',
-            state: 'sales.debtByOutlet',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Выручка',
-            state: 'sales.cashing',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Клиенты',
-            state: 'sales.territory',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Акции',
-            state: 'sales.campaigns',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Фотоотчёты',
-            state: 'sales.photoReports',
-            needRoles: ['admin', 'tester']
-            // needRoles: SALES_ROLES
-          }, {
-            title: 'Визиты',
-            state: 'sales.visits',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Фотопоток',
-            state: 'photos.stream',
-            needRoles: SALES_ROLES
-          }, {
-            title: 'Предзаказы',
-            state: 'sales.prePreOrders',
-            needRoles: 'pre-ordering'
-          }, {
-            title: 'Тесты',
-            state: 'playground',
-            needRoles: ['admin', 'tester']
-          }]
-
-        };
-
-        _.remove(menu.items, function (option) {
-          return !Auth.isAuthorized(option.needRoles);
-        });
-
-        return menu;
-      }
+    function setItemData(state, data) {
+      _.assign (_.find(items, {state}), data);
     }
+
+    function root() {
+
+      return {
+
+        title: 'Начало',
+        state: 'home',
+
+        items: _.filter(items, function (option) {
+          return Auth.isAuthorized(option.needRoles);
+        })
+
+      };
+
+    }
+
+
+    return {
+      root,
+      setItemData
+    };
 
   });
 
