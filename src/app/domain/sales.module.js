8a2906224bcc5b0f36aaa22617df9d359398537e
Alexander Levin <alevin.ru@gmail.com>
2017-04-25 18:14:27 +0300

    show drafts count as menu badge

--- src/app/domain/sales.module.js
+++ src/app/domain/sales.module.js
@@ -5,7 +5,7 @@
   let SUBSCRIPTIONS = ['Stock', 'SaleOrder', 'SaleOrderPosition'];
 
   angular.module('Sales', ['sistemium', 'yaMap', 'Models'])
-    .run(function (SalesmanAuth, InitService, Sockets, IOS, DEBUG, Schema) {
+    .run(function (SalesmanAuth, InitService, Sockets, IOS, DEBUG, Schema, $rootScope, Menu) {
 
       InitService.then(SalesmanAuth.init)
         .then(salesmanAuth => {
@@ -20,13 +20,25 @@
             Sockets.jsDataSubscribe(SUBSCRIPTIONS);
           }
 
-          const Workflow = Schema.model('Workflow');
+          const {Workflow, SaleOrder} = Schema.models();
 
           Workflow.findAll({code: 'SaleOrder.v2'})
             .then(workflow => {
               Schema.workflowSaleOrder = _.get(_.first(workflow), 'workflow');
             })
             .catch(e => console.error('Workflow find error:', e));
+
+          function setBadges() {
+            let filter = SalesmanAuth.makeFilter({processing: 'draft'});
+            SaleOrder.groupBy(filter)
+              .then(data => {
+                Menu.setItemData('sales.saleOrders', {badge: data.length});
+              });
+          }
+
+          $rootScope.$on('menu-show', setBadges);
+
+          setBadges();
 
         });
 
