1a5934c35e5f335065d4336ee4059b93844a8d73
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-12 15:53:36 +0300

    request weight from weighing proxy

--- src/app/providers/weighing.service.js
+++ src/app/providers/weighing.service.js
@@ -2,11 +2,29 @@
 
   angular.module('webPage').service('WeighingService', WeighingService);
 
-  function WeighingService() {
+  function WeighingService($http) {
 
     function weighing() {
 
       console.info('start weighing');
+
+      $http({
+        method: 'GET',
+        url: 'http://localhost:4000/async'
+      })
+        .then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+
+          console.info('successCallback', response);
+
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+
+          console.info('errorCallback', response);
+
+        });
 
     }
 
