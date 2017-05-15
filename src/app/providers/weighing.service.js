24ac16ba0351da0da9100e1c11781342fa21e7a4
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-15 10:45:06 +0300

    comment out callbacks on weighing http promise

--- src/app/providers/weighing.service.js
+++ src/app/providers/weighing.service.js
@@ -8,23 +8,24 @@
 
       console.info('start weighing');
 
-      $http({
+      return $http({
         method: 'GET',
         url: 'http://localhost:4000/async'
       })
-        .then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-
-          console.info('successCallback', response);
-
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-
-          console.info('errorCallback', response);
-
-        });
+      //   .then(function successCallback(response) {
+      //   // this callback will be called asynchronously
+      //   // when the response is available
+      //
+      //     console.info('successCallback', response);
+      //
+      // }, function errorCallback(response) {
+      //   // called asynchronously if an error occurs
+      //   // or server returns response with an error status.
+      //
+      //     console.info('errorCallback', response);
+      //
+      //   })
+      ;
 
     }
 
