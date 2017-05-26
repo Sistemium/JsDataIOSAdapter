b333d350682bdc6263f39be8555f28ceaab26410
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-26 18:36:53 +0300

    replace old code with new one

--- src/app/domain/shipping/shippingPhotos/shippingPhotos.controller.js
+++ src/app/domain/shipping/shippingPhotos/shippingPhotos.controller.js
@@ -2,74 +2,54 @@
 
 (function () {
 
-  function ShippingPhotos(Schema, Helpers, $scope, GalleryHelper, Sockets) {
+  function ShippingPhotos(Schema, Helpers, $scope, GalleryHelper/*, Sockets*/) {
 
     const {ShipmentRoute, ShipmentRoutePoint, ShipmentRoutePointPhoto} = Schema.models();
-    const {saControllerHelper, toastr} = Helpers;
+    const {saControllerHelper/*, toastr*/} = Helpers;
 
     const vm = saControllerHelper.setup(this, $scope)
       .use(GalleryHelper)
       .use({
 
-        isPopoverOpen: false,
+        selectedRoute: undefined,
+        selectedRoutePoint: undefined,
 
-        addItemClick,
-        thumbClick,
-        deleteClick
+        selectRoute
 
       });
 
-    // SalesmanAuth.watchCurrent($scope, refresh);
     refresh();
-
-    $scope.$on('$destroy', Sockets.jsDataSubscribe(['ShipmentRoutePointPhoto']));
-    $scope.$on('$destroy', Sockets.onJsData('jsData:update', onJSData));
 
     /*
      Functions
      */
 
-    function onJSData(event) {
-
-      if (event.resource !== 'ShipmentRoutePointPhoto') return;
-
-      let {data} = event;
-
-      if (!_.get(data, 'href')) return;
-
-      ShipmentRoutePointPhoto.inject(data);
-
-    }
-
-    function deleteClick(picture) {
-      ShipmentRoutePointPhoto.destroy(picture);
-    }
-
-    function thumbClick(picture) {
-
-      vm.commentText = picture.campaign.name;
-      $scope.imagesAll = vm.data;
-
-      return vm.thumbnailClick(picture);
-
-    }
-
-    function addItemClick() {
-      toastr.info('Добавить Фото-отчет');
-    }
-
     function refresh() {
 
-      let q = [
-        ShipmentRoute.findAll(),
-        ShipmentRoutePoint.findAll()
-          .then(() => {
-            return ShipmentRoutePointPhoto.findAllWithRelations({}, {bypassCache: true})(['ShipmentRoutePoint']);
-          })
-      ];
+      if (vm.selectedRoutePoint) return findPhotos();
+      if (vm.selectedRoute) return findPoints();
+      findRoute();
 
+    }
+
+    function findRoute() {
+
+      let q = [
+        ShipmentRoute.findAllWithRelations({limit: 10})('ShipmentRoutePoint').then(routes => vm.data = routes)
+      ];
       vm.setBusy(q);
-      vm.rebindAll(ShipmentRoutePointPhoto, {orderBy: [['deviceCts', 'DESC']]}, 'vm.data');
+
+    }
+
+    function selectRoute(route) {
+      vm.selectedRoute = route;
+    }
+
+    function findPoints(route) {
+
+    }
+
+    function findPhotos(routePoint) {
 
     }
 
