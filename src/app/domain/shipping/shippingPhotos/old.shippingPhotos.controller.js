57a3139ca0abfa751cfbf9a133ab642e04decb15
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-26 15:22:18 +0300

    rename files

---
+++ src/app/domain/shipping/shippingPhotos/old.shippingPhotos.controller.js
@@ -0,0 +1,81 @@
+'use strict';
+
+(function () {
+
+  function ShippingPhotos(Schema, Helpers, $scope, GalleryHelper, Sockets) {
+
+    const {ShipmentRoute, ShipmentRoutePoint, ShipmentRoutePointPhoto} = Schema.models();
+    const {saControllerHelper, toastr} = Helpers;
+
+    const vm = saControllerHelper.setup(this, $scope)
+      .use(GalleryHelper)
+      .use({
+
+        isPopoverOpen: false,
+
+        addItemClick,
+        thumbClick,
+        deleteClick
+
+      });
+
+    // SalesmanAuth.watchCurrent($scope, refresh);
+    refresh();
+
+    $scope.$on('$destroy', Sockets.jsDataSubscribe(['ShipmentRoutePointPhoto']));
+    $scope.$on('$destroy', Sockets.onJsData('jsData:update', onJSData));
+
+    /*
+     Functions
+     */
+
+    function onJSData(event) {
+
+      if (event.resource !== 'ShipmentRoutePointPhoto') return;
+
+      let {data} = event;
+
+      if (!_.get(data, 'href')) return;
+
+      ShipmentRoutePointPhoto.inject(data);
+
+    }
+
+    function deleteClick(picture) {
+      ShipmentRoutePointPhoto.destroy(picture);
+    }
+
+    function thumbClick(picture) {
+
+      vm.commentText = picture.campaign.name;
+      $scope.imagesAll = vm.data;
+
+      return vm.thumbnailClick(picture);
+
+    }
+
+    function addItemClick() {
+      toastr.info('Добавить Фото-отчет');
+    }
+
+    function refresh() {
+
+      let q = [
+        ShipmentRoute.findAll(),
+        ShipmentRoutePoint.findAll()
+          .then(() => {
+            return ShipmentRoutePointPhoto.findAllWithRelations({}, {bypassCache: true})(['ShipmentRoutePoint']);
+          })
+      ];
+
+      vm.setBusy(q);
+      vm.rebindAll(ShipmentRoutePointPhoto, {orderBy: [['deviceCts', 'DESC']]}, 'vm.data');
+
+    }
+
+  }
+
+  angular.module('webPage')
+    .controller('ShippingPhotos', ShippingPhotos);
+
+}());
