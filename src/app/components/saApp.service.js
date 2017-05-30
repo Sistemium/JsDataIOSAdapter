3a677c6bf48265ffbcec61970d88d61b10a7c441
Alexander Levin <alevin.ru@gmail.com>
2017-05-30 19:50:27 +0300

    debts xxs fix

--- src/app/components/saApp.service.js
+++ src/app/components/saApp.service.js
@@ -5,7 +5,7 @@
   function saApp($window, Schema, UUID) {
 
     const appIdKey = 'saAppId';
-    const VERSION = '0.11.3';
+    const VERSION = '0.11.4';
 
     let appId = $window.localStorage.getItem(appIdKey);
 
