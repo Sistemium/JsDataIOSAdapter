3f4d0d7f23fe4a93be7a67d51d2e3add39b97818
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-04 11:22:29 +0300

    add shipmentRoutes.controller

---
+++ src/app/domain/shipping/shipmentRoutes.controller.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+
+  function ShipmentRoutes(Schema, Helpers, $scope, $state, SalesmanAuth) {
+
+    const {ShipmentRoute} = Schema.models();
+    const {saControllerHelper} = Helpers;
+
+    const vm = saControllerHelper.setup(this, $scope)
+      .use({
+        selectRoute
+      });
+
+    let filter = SalesmanAuth.makeFilter({limit: 10, orderBy: [['date', 'DESC']]});
+
+    let q = [
+      ShipmentRoute.findAllWithRelations(filter)('ShipmentRoutePoint').then(routes => vm.data = routes)
+    ];
+    vm.setBusy(q);
+
+    function selectRoute(route) {
+      $state.go('.', {routeId: route.id});
+    }
+
+  }
+
+  angular.module('webPage')
+    .controller('ShipmentRoutes', ShipmentRoutes);
+
+}());
