30b1a60158e62db46826fbc28d1267e4e4d57cdc
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-09 15:04:21 +0300

    rm SalesmanAuth.makeFilter from ShipmentRoute.findAll

--- src/app/domain/shipping/shipmentRoutes.controller.js
+++ src/app/domain/shipping/shipmentRoutes.controller.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  function ShipmentRoutes(Schema, Helpers, $scope, $state, SalesmanAuth) {
+  function ShipmentRoutes(Schema, Helpers, $scope, $state) {
 
     const {ShipmentRoute} = Schema.models();
     const {saControllerHelper} = Helpers;
@@ -12,12 +12,10 @@
         selectRoute
       });
 
-    let filter = SalesmanAuth.makeFilter();
-
     let options = {limit: 10, orderBy: [['date', 'DESC']]};
 
     let q = [
-      ShipmentRoute.findAllWithRelations(filter, options)('ShipmentRoutePoint').then(routes => vm.data = routes)
+      ShipmentRoute.findAllWithRelations({}, options)('ShipmentRoutePoint').then(routes => vm.data = routes)
     ];
     vm.setBusy(q);
 
