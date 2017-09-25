d279c8ba271a580abc206111ca0cbdf87fca8eb8
Alexander Levin <alevin.ru@gmail.com>
2017-09-25 10:56:31 +0300

    show summOrigin in debt

--- src/app/components/saApp.service.js
+++ src/app/components/saApp.service.js
@@ -5,7 +5,7 @@
   function saApp($window, Schema, UUID) {
 
     const appIdKey = 'saAppId';
-    const VERSION = '0.15.1';
+    const VERSION = '0.15.2';
 
     let appId = $window.localStorage.getItem(appIdKey);
 
