417a53d4ff75190dc11201d1a9e303c79d9ee1df
Alexander Levin <alevin.ru@gmail.com>
2017-10-09 15:14:43 +0300

    photoReports with filtering

--- src/app/domain/sales/photoReports/PhotoReportList.controller.js
+++ src/app/domain/sales/photoReports/PhotoReportList.controller.js
@@ -2,16 +2,21 @@
 
   module.controller('PhotoReportListController', PhotoReportListController);
 
-  function PhotoReportListController(Schema, Helpers, $scope, SalesmanAuth, GalleryHelper, Sockets) {
+  function PhotoReportListController(Schema, Helpers, $scope, SalesmanAuth, GalleryHelper, Sockets, localStorageService) {
 
     const {PhotoReport, Outlet, Campaign, CampaignGroup} = Schema.models();
     const {saControllerHelper, toastr} = Helpers;
+
+    const LOCAL_STORAGE_KEY = 'photoReportForm.defaults';
 
     const vm = saControllerHelper.setup(this, $scope)
       .use(GalleryHelper)
       .use({
 
         isPopoverOpen: false,
+        campaignGroups: [],
+        campaigns: [],
+        outlets: [],
 
         addItemClick,
         thumbClick,
@@ -23,6 +28,11 @@
 
     $scope.$on('$destroy', Sockets.jsDataSubscribe(['PhotoReport']));
     $scope.$on('$destroy', Sockets.onJsData('jsData:update', onJSData));
+
+    vm.onScope('rootClick', () => {
+      vm.campaignId = null;
+      vm.outletId = null;
+    });
 
     /*
      Functions
@@ -57,6 +67,36 @@
       toastr.info('Добавить Фото-отчет');
     }
 
+    function loadPhotoReports() {
+
+      let filter = {orderBy: [['deviceCts', 'DESC']]};
+
+      let {campaignId, outletId} = vm;
+
+      if (vm.campaignId) {
+        filter.campaignId = vm.campaignId;
+      }
+
+      if (vm.outletId) {
+        filter.outletId = vm.outletId;
+      }
+
+      vm.rebindAll(PhotoReport, filter, 'vm.data');
+
+      let q = PhotoReport.findAllWithRelations(filter, {bypassCache: true})(['Outlet']);
+
+      vm.setBusy(q);
+
+      if (campaignId && outletId) {
+        vm.photoReport = PhotoReport.createInstance({campaignId, outletId})
+      } else {
+        vm.photoReport = null;
+      }
+
+    }
+
+    let unWatchRefresh;
+
     function refresh() {
 
       let filter = SalesmanAuth.makeFilter();
@@ -65,13 +105,60 @@
         CampaignGroup.findAll(),
         Campaign.findAll(),
         Outlet.findAll(Outlet.meta.salesmanFilter(filter))
-          .then(() => {
-            return PhotoReport.findAllWithRelations({}, {bypassCache: true})(['Outlet']);
-          })
+          .then(data => vm.outlets = data)
+          .then(loadFiltersData)
       ];
 
       vm.setBusy(q);
-      vm.rebindAll(PhotoReport, {orderBy: [['deviceCts', 'DESC']]}, 'vm.data');
+
+      if (unWatchRefresh) {
+        unWatchRefresh();
+      }
+
+      unWatchRefresh = $scope.$watchGroup(['vm.campaignId', 'vm.outletId', 'vm.campaignGroupId'], loadPhotoReports);
+
+    }
+
+    function loadFiltersData() {
+
+      return CampaignGroup.findAll()
+        .then(groups => {
+
+          vm.campaignGroups = _.filter(groups, campaignGroup => {
+            return moment().isAfter(campaignGroup.dateB) && moment().add(-90, 'days').isBefore(campaignGroup.dateE);
+          });
+
+          vm.campaignGroupId = localStorageService.get(`${LOCAL_STORAGE_KEY}.campaignGroupId`);
+
+          if (!vm.campaignGroupId) {
+            let today = moment().format();
+            vm.campaignGroupId = _.get(_.find(groups, group => group.dateB <= today && today <= group.dateE), 'id');
+          }
+
+          $scope.$watch('vm.campaignGroupId', campaignGroupId => {
+
+            vm.campaignGroup = CampaignGroup.get(campaignGroupId);
+
+            if (!campaignGroupId) {
+              vm.campaigns = [];
+              return;
+            }
+
+            Campaign.findAll(Campaign.meta.filterByGroup(vm.campaignGroup))
+              .then(campaigns => {
+
+                vm.campaigns = campaigns;
+
+                if (vm.campaignId && !_.find(campaigns, {id: vm.campaignId})) {
+                  vm.campaignId = null;
+                }
+
+              });
+
+          });
+
+        });
+
 
     }
 
