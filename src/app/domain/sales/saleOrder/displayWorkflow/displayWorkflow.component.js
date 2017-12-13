de50440b13bd3605e0fca6e97ef2259e96678423
TaWReOn <deniss.mosin@gmail.com>
2017-12-13 18:17:08 +0200

    displayWorkflow component

--- src/app/domain/sales/saleOrder/displayWorkflow/displayWorkflow.component.js
+++ src/app/domain/sales/saleOrder/displayWorkflow/displayWorkflow.component.js
@@ -6,9 +6,8 @@
       templateUrl: 'app/domain/sales/saleOrder/displayWorkflow/displayWorkflow.html',
 
       bindings: {
-        workflows: '<',
-        supplementaryWorkflowData: '<',
-        currentWorkflow: '='
+        currentWorkflow: '=',
+        workflowsInPromise: '<'
       },
 
       controller: WorkflowController,
@@ -16,16 +15,125 @@
 
     });
 
-  function WorkflowController(saControllerHelper, $scope) {
+  function WorkflowController(saControllerHelper, $scope, localStorageService, Schema) {
 
     const vm = saControllerHelper.setup(this, $scope);
+    let {Workflow} = Schema.models();
 
     vm.use({
-      onWorkflowChange
+
+      currentWorkflows: {},
+      workflowDictionary: {},
+      translatedWorkflow: '',
+      dropdownClicked: null,
+
+      onWorkflowChange,
+      $onInit
     });
 
+    $scope.$watch('vm.workflowsInPromise', () => {
+      loadCurrentWorkflows();
+    });
+
+    function $onInit() {
+
+      if (!vm.currentWorkflow) {
+        vm.currentWorkflow = localStorageService.get('currentWorkflow') || null;
+      }
+
+    }
+
+    function findTranslation(workflow, isInit) {
+
+      let initMsg = 'Выберите статус';
+
+      if (!isInit) {
+        if (workflow === vm.currentWorkflow) {
+          vm.translatedWorkflow = initMsg;
+          return;
+        }
+      }
+
+      if (workflow === 'bad') {
+        vm.translatedWorkflow = 'bad';
+        return;
+      }
+
+      let translationWorkflow = _.find(vm.workflowDictionary, el => {
+        return el.workflowName === workflow;
+      });
+
+      vm.translatedWorkflow = _.get(translationWorkflow, 'translation') || initMsg;
+
+    }
+
     function onWorkflowChange(workflow) {
-      vm.currentWorkflow = _.get(workflow, 'processing');
+
+      findTranslation(workflow);
+
+      if (vm.currentWorkflow === workflow) {
+        localStorageService.set('currentWorkflow', null);
+        vm.currentWorkflow = null;
+      } else {
+        localStorageService.set('currentWorkflow', workflow);
+        vm.currentWorkflow = workflow;
+      }
+
+    }
+
+    function loadSupplementaryWorkflowData() {
+
+      Workflow.findAll({code: 'SaleOrder.v2'})
+        .then((res) => {
+          let workflowTranslations = _.get(res[0], 'workflow');
+
+          let mergedWorkflows = _.merge(workflowTranslations, vm.currentWorkflows);
+
+          _.each(mergedWorkflows, (workflow, key) => {
+
+            vm.workflowDictionary.push({
+              workflowName: key,
+              translation: _.get(workflow, 'label'),
+              cls: _.get(workflow, 'cls'),
+              cnt: _.get(vm.currentWorkflows[key], 'cnt') || 0
+            });
+
+          });
+
+          vm.workflowDictionary = _.orderBy(vm.workflowDictionary, ['cnt'], ['desc']);
+
+          findTranslation(vm.currentWorkflow, true);
+
+        })
+
+    }
+
+    function loadCurrentWorkflows() {
+
+      vm.currentWorkflows = {};
+      vm.workflowDictionary = [];
+
+      vm.workflowsInPromise
+        .then((res) => {
+
+          _.each(res, item => {
+
+            let workflow = _.get(item, 'processing');
+
+            let obj = {
+              [workflow]: {
+                cnt: _.get(item, 'count()')
+              }
+            };
+
+            _.assign(vm.currentWorkflows, obj);
+
+          });
+
+          loadSupplementaryWorkflowData();
+
+        })
+
     }
 
   }
