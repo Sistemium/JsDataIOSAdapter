d5cf5427826150137fbcd46d2157856cbb1c583e
Alexander Levin <alevin.ru@gmail.com>
2017-12-14 17:43:10 +0200

    showNewsCarousel for r50p

--- src/app/domain/components/DomainOption.service.js
+++ src/app/domain/components/DomainOption.service.js
@@ -20,7 +20,7 @@
     };
 
     function showNewsCarousel() {
-      return customerCode() === 'r50' && Auth.isAuthorized(['salesman', 'newsMaker']);
+      return customerCode().match(/r50?/) && Auth.isAuthorized(['salesman', 'newsMaker']);
     }
 
     function saleOrderOptions() {
