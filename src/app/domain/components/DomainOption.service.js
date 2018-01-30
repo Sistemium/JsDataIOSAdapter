92c3b4e0ed77c0acbd45bf6de2a24cc94103567d
Alexander Levin <alevin.ru@gmail.com>
2018-01-30 11:40:30 +0200

    DomainOption saleOrdersDisabled not for dev

--- src/app/domain/components/DomainOption.service.js
+++ src/app/domain/components/DomainOption.service.js
@@ -89,7 +89,7 @@
     }
 
     function saleOrdersDisabled() {
-      return customerCode() === 'bs';
+      return customerCode() === 'bs' === _.get(Auth.getAccount(), 'org');
     }
 
     function visitsDisabled() {
