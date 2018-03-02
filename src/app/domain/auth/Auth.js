c4ea63304b20e240f63668e363d32b4a0c8aa635
Alexander Levin <alevin.ru@gmail.com>
2018-03-02 08:11:21 +0200

    formatting

--- src/app/domain/auth/Auth.js
+++ src/app/domain/auth/Auth.js
@@ -14,6 +14,93 @@
 
     const ios = IOS.isIos();
 
+    $rootScope.$on('$stateChangeStart', onStateChangeStart);
+
+    $rootScope.$on('authenticated', onAuthenticated);
+
+    return _.assign(me, {
+
+      getCurrentUser: () => {
+        return me.profileState === 'picker' && PickerAuth.getCurrentUser() || currentUser;
+      },
+
+      getAccount: () => {
+        return currentUser;
+      },
+
+      isLoggedIn: () => {
+        return !!currentUser;
+      },
+
+      isAdmin: function () {
+        return isAuthorized(['admin', 'tester']);
+      },
+
+      init,
+
+      getAccessToken,
+
+      isAuthorized,
+
+      authId: function () {
+        return currentUser.authId;
+      },
+
+      roles: () => {
+        return roles && roles.roles;
+      },
+
+      role: code => {
+        return _.get(roles.roles, code);
+      }
+
+    });
+
+    /*
+    Functions
+     */
+
+    function onAuthenticated(event, res) {
+      console.log('authenticated', res);
+      setRoles(res);
+      if (resolveRoles) {
+        resolveRoles(roles);
+      }
+      $window.localStorage.setItem('authorization', res.accessToken);
+    }
+
+    function onStateChangeStart(event, next, nextParams, from) {
+
+      if (!roles) {
+        if (next.name !== 'auth') {
+
+          event.preventDefault();
+          if (rolesPromise) {
+            rolesPromise.then(() => {
+              $state.go(next, nextParams);
+            });
+            return;
+          }
+
+          $state.go('auth');
+
+        }
+      } else {
+        me.profileState = 'profile';
+        if (_.get(next, 'data.auth') === 'pickerAuth') {
+          me.profileState = 'picker';
+        }
+      }
+
+      let needRoles = _.get(next, 'data.auth');
+
+      if (needRoles && !event[needRoles]) {
+        event.preventDefault();
+        console.info(`Should be prevented state change to ${next.name} from ${from.name} by ${needRoles}`);
+      }
+
+    }
+
     function getAccessToken() {
       return ios || $window.localStorage.getItem('authorization');
     }
@@ -28,11 +115,11 @@
       currentUser = roles.account || {};
 
       currentUser.shortName = (name => {
-        const names = name.match (/(^[^ ]*) (.*$)/);
+        const names = name.match(/(^[^ ]*) (.*$)/);
         return names ? names[1] + ' ' + names[2][0] + '.' : name;
       })(currentUser.name);
 
-      rolesArray = _.map(roles.roles, (val,key) => {
+      rolesArray = _.map(roles.roles, (val, key) => {
         return key;
       });
 
@@ -63,7 +150,7 @@
 
         rolesPromise = authProtocol.getRoles(token)
           .then(res => {
-            console.log ('Auth.init',res);
+            console.log('Auth.init', res);
             return setRoles(res);
           });
 
@@ -95,86 +182,6 @@
         !!_.intersection(anyRoles, rolesArray).length
         ;
     }
-
-    $rootScope.$on('$stateChangeStart', (event, next, nextParams, from) => {
-
-      if (!roles) {
-        if (next.name !== 'auth') {
-
-          event.preventDefault();
-          if (rolesPromise) {
-            rolesPromise.then(() => {
-              $state.go(next, nextParams);
-            });
-            return;
-          }
-
-          $state.go('auth');
-
-        }
-      } else {
-        me.profileState = 'profile';
-        if (_.get(next, 'data.auth') === 'pickerAuth') {
-          me.profileState = 'picker';
-        }
-      }
-
-      let needRoles = _.get(next, 'data.auth');
-
-      if (needRoles && !event[needRoles]) {
-        event.preventDefault();
-        console.info(`Should be prevented state change to ${next.name} from ${from.name} by ${needRoles}`);
-      }
-
-    });
-
-    $rootScope.$on('authenticated', (event, res) => {
-      console.log ('authenticated', res);
-      setRoles(res);
-      if (resolveRoles) {
-        resolveRoles(roles);
-      }
-      $window.localStorage.setItem('authorization', res.accessToken);
-    });
-
-    return angular.extend(me, {
-
-      getCurrentUser: () => {
-        return me.profileState === 'picker' && PickerAuth.getCurrentUser() || currentUser;
-      },
-
-      getAccount: () => {
-        return currentUser;
-      },
-
-      isLoggedIn: () => {
-        return !!currentUser;
-      },
-
-      isAdmin: function () {
-        return isAuthorized(['admin', 'tester']);
-      },
-
-      init,
-
-      getAccessToken,
-
-
-      isAuthorized,
-
-      authId: function() {
-        return currentUser.authId;
-      },
-
-      roles: () => {
-        return roles && roles.roles;
-      },
-
-      role: code => {
-        return _.get(roles.roles, code);
-      }
-
-    });
 
   }
 
