d6595c333bbd16e790c099d0a98d3f37d9f6588f
TaWReOn <deniss.mosin@gmail.com>
2018-03-06 16:13:54 +0200

    models

--- src/app/domain/models/Ordering/ArticleTag.model.js
+++ src/app/domain/models/Ordering/ArticleTag.model.js
@@ -4,35 +4,6 @@
 
   function ArticleTag(Schema) {
 
-    const model = Schema.register({
-
-      name: 'ArticleTag',
-
-      meta: {
-
-        tags: [
-          ['age3', '3 года', /[^0-9]3 года|тр[её]хлетний/, 'age'],
-          ['age4', '4 года', /[^0-9]4 года|четыр[её]хлетний/, 'age'],
-          ['age5', '5 лет', /[^0-9]5 лет|пятилетний/, 'age'],
-
-          ['rose', 'розовое', /розовый|розовое|розов\./i, 'color'],
-          ['red', 'красное', /красн\.|красное|кр\.(?![^\d][\d]+)/i, 'color'],
-          ['white', 'белое', /([^A-я]|^)бел[. ]|белое|белый/i, 'color'],
-
-          ['semiDry', 'п/сух', /полусухое|п\/сух\.?/ig, 'taste'],
-          ['semiSweet', 'п/сл', /п\/слад\.|полуслад[^ ,"]*|п\/сл[,.]+|п\/сл(?=[ ]|$)/ig, 'taste'],
-          ['dry', 'сухое', /сухое|сух[.,]+|[ .,]+сух(?=[ ]|$)/i, 'taste'],
-          ['sweet', 'сладкое', /([ ]|^)+сладк[^ ,"]*|сладкое|сл\./i, 'taste'],
-          ['brut', 'брют', /брют/i, 'taste'],
-
-          ['sparkling', 'игристое', /игристое/i, null],
-          ['gift', 'п/у', /подар[^ .]*|под[^ .]*[ .]{1,2}упа[^ .)]*|в п\/у[^ .)]*|п\/у[^ .)]*/i, null]
-        ]
-
-      }
-
-    });
-
     const tags = [
       ['age3', '3 года', /[^0-9]3 года|тр[её]хлетний/, 'age'],
       ['age4', '4 года', /[^0-9]4 года|четыр[её]хлетний/, 'age'],
@@ -48,15 +19,35 @@
       ['sweet', 'сладкое', /([ ]|^)+сладк[^ ,"]*|сладкое|сл\./i, 'taste'],
       ['brut', 'брют', /брют/i, 'taste'],
 
-      ['sparkling', 'игристое', /игристое/i, null],
-      ['gift', 'п/у', /подар[^ .]*|под[^ .]*[ .]{1,2}упа[^ .)]*|в п\/у[^ .)]*|п\/у[^ .)]*/i, null]
+      ['sparkling', 'игристое', /игристое/i, 'other'],
+      ['gift', 'п/у', /подар[^ .]*|под[^ .]*[ .]{1,2}упа[^ .)]*|в п\/у[^ .)]*|п\/у[^ .)]*/i, 'other']
     ];
+
+    const model = Schema.register({
+
+      name: 'ArticleTag',
+
+      relations: {
+        hasOne: {
+          ArticleTagGroup: {
+            localField: 'group',
+            localKey: 'groupId'
+          }
+        }
+      },
+
+      meta: {
+        tags
+      }
+
+    });
+
 
     _.each(tags, tag => {
 
-      let [id, label, re, group] = tag;
+      let [id, label, re, groupId] = tag;
 
-      model.inject({id, label, re, group});
+      model.inject({id, label, re, groupId});
 
     });
 
---
+++ src/app/domain/models/Ordering/ArticleTagGroup.model.js
@@ -0,0 +1,42 @@
+(function () {
+
+  angular.module('Models')
+    .run(ArticleTagGroup);
+
+  function ArticleTagGroup(Schema) {
+
+    const data = [
+      ['age', 'Выдержка'],
+      ['color', 'Цвет'],
+      ['taste', 'Вкус'],
+      ['other', 'Прочее', true]
+    ];
+
+    const model = Schema.register({
+
+      name: 'ArticleTagGroup',
+
+      relations: {
+        hasMany: {
+          ArticleTag: {
+            localField: 'tags',
+            foreignKey: 'groupId'
+          }
+        }
+      },
+
+      meta: {}
+
+    });
+
+    _.each(data, (item, idx) => {
+
+      let [id, name, allowMultiple = false] = item;
+
+      model.inject({id, name, allowMultiple, ord: idx + 1});
+
+    });
+
+  }
+
+})();
