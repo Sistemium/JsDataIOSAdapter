7cf6caf02392ecd4fc1f8c5d4f1bda05700d9b5a
TaWReOn <deniss.mosin@gmail.com>
2018-03-27 11:28:45 +0300

    catalogueSearchButtons test on playground

--- src/app/domain/playground/playground.controller.js
+++ src/app/domain/playground/playground.controller.js
@@ -5,43 +5,7 @@
     .module('webPage')
     .controller('PlayGroundController', PlayGroundController);
 
-  function PlayGroundController(Schema, DEBUG, moment) {
-
-    const vm = this;
-    const {Price, Article, ArticleGroup} = Schema.models();
-
-    let lastFinished = vm.started = moment();
-    vm.finished = {};
-
-    DEBUG('PlayGroundController started', vm.started);
-
-    Price
-      .findAll({}, options({afterFindAll}))
-      .then(() => {
-        vm.finished['Price'] = moment().diff(lastFinished);
-        lastFinished = moment();
-      })
-      .then(() => Article.cachedFindAll({}, options({afterFindAll})))
-      .then(() => {
-        vm.finished['Article'] = moment().diff(lastFinished);
-        lastFinished = moment();
-      })
-      .then(() => ArticleGroup.cachedFindAll({}, options()))
-      .then(() => {
-        vm.finished['ArticleGroup'] = moment().diff(lastFinished);
-        lastFinished = moment();
-      })
-      .catch(err => vm.err = err);
-
-    function afterFindAll(options, data) {
-      DEBUG('PlayGroundController afterFindAll', options, data);
-      return [];
-      // return data;
-    }
-
-    function options(o) {
-      return _.assign({limit: 10000, bypassCache: true}, o);
-    }
+  function PlayGroundController() {
 
   }
 
