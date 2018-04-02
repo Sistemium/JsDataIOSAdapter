2aecfa2632673e52b55b6819d6e5a433c788b388
Alexander Levin <alevin.ru@gmail.com>
2018-04-02 15:50:51 +0300

    v0.18.11

--- src/app/components/saApp.service.js
+++ src/app/components/saApp.service.js
@@ -5,7 +5,7 @@
   function saApp($window, Schema, UUID) {
 
     const appIdKey = 'saAppId';
-    const VERSION = '0.18.10';
+    const VERSION = '0.18.11';
 
     let appId = $window.localStorage.getItem(appIdKey);
 
