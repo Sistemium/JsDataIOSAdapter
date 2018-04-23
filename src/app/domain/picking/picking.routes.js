1560e431e08f2eb182e51cd0db1742e3b254cc12
Alexander Levin <alevin.ru@gmail.com>
2018-04-23 06:56:37 +0300

    formatting

--- src/app/domain/picking/picking.routes.js
+++ src/app/domain/picking/picking.routes.js
@@ -3,102 +3,130 @@
 (function () {
 
   angular.module('webPage')
-    .config(function (stateHelperProvider) {
+    .config(pickingRoutes);
 
-      stateHelperProvider.state({
-        name: 'picker',
-        url: '/picker',
-        templateUrl: 'app/domain/picker/info.html',
-        controller: 'PickerInfoController',
-        controllerAs: 'vm'
-      });
+  function pickingRoutes(stateHelperProvider) {
 
-      var articleChildren = [
-        {
-          name: 'article',
-          url: '/:id',
-          templateUrl: 'app/domain/picking/pickingArticle.html',
-          controller: 'PickingArticleController',
-          controllerAs: 'vm',
-          data: {
-            hideBottomBar: true
-          },
-          children: [
-            {
-              name: 'pick',
-              url: '/pick/:positionId',
-              templateUrl: 'app/domain/picking/pickPosition.html',
-              controller: 'PickedPositionController',
-              controllerAs: 'vm'
-            },{
-              name: 'pickedPosition',
-              url: '/picked/:pickedPositionId',
-              templateUrl: 'app/domain/picking/pickPosition.html',
-              controller: 'PickedPositionController',
-              controllerAs: 'vm'
-            }
-          ]
-        }
-      ];
+    stateHelperProvider.state({
 
-      stateHelperProvider
+      name: 'picker',
+      url: '/picker',
 
-        .state({
-          name: 'picking',
-          abstract: true,
-          template: '<div class="domain-ui-view picking" ui-view></div>',
-          data: {
-            auth: 'pickerAuth'
-          },
-          children: [
-            { name:'orderList',
-              url: '/orders?state',
-              templateUrl: 'app/domain/picking/pickingOrderList.html',
-              controller: 'PickingOrderListController',
-              controllerAs: 'vm',
-              data: {
-                title: 'Сборка требований'
-              },
-              children: [
-                {
-                  name: 'selectedOrders',
-                  url: '/selected',
-                  templateUrl: 'app/domain/picking/selectedOrders.html',
-                  controller: 'SelectedOrdersController',
-                  controllerAs: 'vm',
-                  data: {
-                    hideTopBar: true
-                  }
-                },{
-                  name: 'articleList',
-                  url: '/articleList',
-                  templateUrl: 'app/domain/picking/articleList.html',
-                  controller: 'ArticleListController',
-                  controllerAs: 'vm',
-                  children: angular.copy (articleChildren),
-                  data: {
-                    needBarcode: true,
-                    hideTopBar: true
-                  }
-                },{
-                  name: 'picked',
-                  url: '/picked',
-                  templateUrl: 'app/domain/picking/articleList.html',
-                  controller: 'ArticleListController',
-                  controllerAs: 'vm',
-                  children: angular.copy (articleChildren),
-                  data: {
-                    needBarcode: true,
-                    hideTopBar: true
-                  }
+      templateUrl: 'app/domain/picker/info.html',
+      controller: 'PickerInfoController',
+      controllerAs: 'vm'
+
+    });
+
+    const articleChildren = [
+      {
+        name: 'article',
+        url: '/:id',
+
+        templateUrl: 'app/domain/picking/pickingArticle.html',
+        controller: 'PickingArticleController',
+        controllerAs: 'vm',
+
+        data: {
+          hideBottomBar: true
+        },
+
+        children: [
+          {
+            name: 'pick',
+            url: '/pick/:positionId',
+
+            templateUrl: 'app/domain/picking/pickPosition.html',
+            controller: 'PickedPositionController',
+            controllerAs: 'vm'
+
+          }, {
+            name: 'pickedPosition',
+            url: '/picked/:pickedPositionId',
+
+            templateUrl: 'app/domain/picking/pickPosition.html',
+            controller: 'PickedPositionController',
+            controllerAs: 'vm'
+          }
+        ]
+      }
+    ];
+
+    stateHelperProvider
+
+      .state({
+
+        name: 'picking',
+        abstract: true,
+        template: '<div class="domain-ui-view picking" ui-view></div>',
+
+        data: {
+          auth: 'pickerAuth'
+        },
+
+        children: [
+          {
+            name: 'orderList',
+            url: '/orders?state',
+
+            templateUrl: 'app/domain/picking/pickingOrderList.html',
+            controller: 'PickingOrderListController',
+            controllerAs: 'vm',
+
+            data: {
+              title: 'Сборка требований'
+            },
+
+            children: [
+              {
+                name: 'selectedOrders',
+                url: '/selected',
+
+                templateUrl: 'app/domain/picking/selectedOrders.html',
+                controller: 'SelectedOrdersController',
+                controllerAs: 'vm',
+
+                data: {
+                  hideTopBar: true
                 }
-              ]
-            }
-          ]
-        })
-      ;
 
-    })
-  ;
+              }, {
+                name: 'articleList',
+                url: '/articleList',
+
+                templateUrl: 'app/domain/picking/articleList.html',
+                controller: 'ArticleListController',
+                controllerAs: 'vm',
+
+                children: angular.copy(articleChildren),
+
+                data: {
+                  needBarcode: true,
+                  hideTopBar: true
+                }
+
+              }, {
+                name: 'picked',
+                url: '/picked',
+
+                templateUrl: 'app/domain/picking/articleList.html',
+                controller: 'ArticleListController',
+                controllerAs: 'vm',
+
+                children: angular.copy(articleChildren),
+
+                data: {
+                  needBarcode: true,
+                  hideTopBar: true
+                }
+
+              }
+            ]
+          }
+        ]
+      })
+    ;
+
+  }
 
 })();
