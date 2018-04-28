ed7412a6fbcbcd3f0a246c59767acbdf4e414340
Alexander Levin <alevin.ru@gmail.com>
2018-04-28 19:44:02 +0300

    playground parallel

--- src/app/domain/playground/playground.controller.js
+++ src/app/domain/playground/playground.controller.js
@@ -5,36 +5,43 @@
     .module('webPage')
     .controller('PlayGroundController', PlayGroundController);
 
-  function PlayGroundController(Schema, DEBUG, moment) {
+  function PlayGroundController(Schema, DEBUG, moment, $q) {
 
     const vm = this;
     const {Price, Article, ArticleGroup} = Schema.models();
 
     let lastFinished = vm.started = moment();
     vm.finished = {};
+    vm.samples = [];
 
     DEBUG('PlayGroundController started', vm.started);
 
-    Price
-      .findAll({}, options({afterFindAll}))
+    $q.all([
+      Price.findAll({}, options({afterFindAll}))
+
+        .then(() => {
+          vm.finished['Price'] = moment().diff(lastFinished);
+          // lastFinished = moment();
+        }),
+      Article.cachedFindAll({}, options({afterFindAll}))
+        .then(() => {
+          vm.finished['Article'] = moment().diff(lastFinished);
+          // lastFinished = moment();
+        }),
+      ArticleGroup.cachedFindAll({}, options())
+        .then(() => {
+          vm.finished['ArticleGroup'] = moment().diff(lastFinished);
+          // lastFinished = moment();
+        })
+    ])
       .then(() => {
-        vm.finished['Price'] = moment().diff(lastFinished);
-        lastFinished = moment();
-      })
-      .then(() => Article.cachedFindAll({}, options({afterFindAll})))
-      .then(() => {
-        vm.finished['Article'] = moment().diff(lastFinished);
-        lastFinished = moment();
-      })
-      .then(() => ArticleGroup.cachedFindAll({}, options()))
-      .then(() => {
-        vm.finished['ArticleGroup'] = moment().diff(lastFinished);
-        lastFinished = moment();
+        vm.finished.total = moment().diff(vm.started);
       })
       .catch(err => vm.err = err);
 
     function afterFindAll(options, data) {
       DEBUG('PlayGroundController afterFindAll', options, data);
+      vm.samples.push(data[0]);
       return [];
       // return data;
     }
