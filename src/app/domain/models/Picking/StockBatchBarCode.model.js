62c53ac2cd611e43b8ad4785f2f734b22469a317
Alexander Levin <alevin.ru@gmail.com>
2018-06-03 03:20:08 +0300

    formatting

--- src/app/domain/models/Picking/StockBatchBarCode.model.js
+++ src/app/domain/models/Picking/StockBatchBarCode.model.js
@@ -2,51 +2,51 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema,$q) {
+  angular.module('Models').run((Schema, $q) => {
 
-      Schema.register ({
+    Schema.register({
 
-        name: 'StockBatchBarCode',
+      name: 'StockBatchBarCode',
 
-        relations: {
-          belongsTo: {
-            StockBatch: {
-              localField: 'StockBatch',
-              localKey: 'stockBatchId'
+      relations: {
+        belongsTo: {
+          StockBatch: {
+            localField: 'StockBatch',
+            localKey: 'stockBatchId'
+          }
+        }
+      },
+
+      someBy: {
+
+        article(id) {
+
+          const SB = Schema.model('StockBatch');
+
+          return $q((resolve, reject) => {
+
+            if (!id) {
+              return reject('Укажите товар');
             }
-          }
-        },
 
-        someBy: {
-
-          article: id => {
-
-            const SB = Schema.model('StockBatch');
-
-            return $q((resolve, reject) => {
-
-              if (!id) {
-                return reject('Укажите товар');
+            SB.findAll({ articleId: id, limit: 1 }).then(sbs => {
+              if (!sbs.length) {
+                return reject();
               }
+              SB.loadRelations(sbs[0], 'StockBatchBarCode')
+                .then(sb => {
+                  resolve(sb.stockBatchBarCodes);
+                }, reject);
+            }, reject);
 
-              SB.findAll({articleId: id, limit: 1}).then(sbs => {
-                if (!sbs.length) {
-                  return reject ();
-                }
-                SB.loadRelations(sbs[0], 'StockBatchBarCode')
-                  .then(sb => {
-                    resolve(sb.stockBatchBarCodes);
-                  }, reject);
-              }, reject);
-
-            });
-
-          }
+          });
 
         }
 
-      });
+      }
 
     });
+
+  });
 
 })();
