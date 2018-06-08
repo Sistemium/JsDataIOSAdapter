500e81de86192cba0be986c1a1baf4e4d8865ac7
Alexander Levin <alevin.ru@gmail.com>
2018-06-08 08:57:46 +0300

    stockTaking.routes separate

--- src/app/domain/warehousing/warehousing.routes.js
+++ src/app/domain/warehousing/warehousing.routes.js
@@ -17,7 +17,7 @@
             // auth: 'SalesmanAuth'
           },
 
-          children: [stockBatching, stockTaking]
+          children: [stockBatching]
 
         });
 
@@ -33,63 +33,6 @@
     },
 
     template: '<stock-batching></stock-batching>'
-
-  };
-
-  const stockTaking = {
-
-    name: 'stockTaking',
-    url: '/stockTaking',
-
-    data: {
-      title: 'Инвентаризация',
-    },
-
-    template: '<stock-taking></stock-taking>',
-
-    children: [{
-      name: 'view',
-      url: '/view/:stockTakingId',
-      template: '<stock-taking-view ' +
-      'ng-model="vm.params.stockTakingId" ' +
-      'item-id="vm.params.stockTakingItemId"' +
-      '></stock-taking-view>',
-      controller: 'StateController as vm',
-      data: {
-        rootState: 'wh.stockTaking',
-        watch: {
-          ['vm.params.stockTakingItemId'](stockTakingItemId, $state) {
-            if (stockTakingItemId) {
-              $state.go('wh.stockTaking.view.item', { stockTakingItemId })
-            } else {
-              $state.go('wh.stockTaking.view')
-            }
-          }
-        },
-      },
-
-      children: [{
-        name: 'item',
-        url: '/:stockTakingItemId',
-        controller: 'StateController as vm',
-        template: '<stock-taking-item-view ng-model="vm.params.stockTakingItemId"></stock-taking-item-view>',
-        data: {
-          on: {
-            ['stock-taking-item-destroy'](stockTakingItem, $state) {
-              $state.go('^');
-            },
-          }
-        }
-      }]
-
-    }, {
-      name: 'create',
-      url: '/create',
-      template: '<stock-taking-view></stock-taking-view>',
-      data: {
-        rootState: 'wh.stockTaking',
-      },
-    },],
 
   };
 
