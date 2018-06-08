bc1d90fb177b43bfa514da58511d4855004152ea
Alexander Levin <alevin.ru@gmail.com>
2018-06-08 10:32:36 +0300

    StateController initLocals

--- src/app/routes/StateController.js
+++ src/app/routes/StateController.js
@@ -3,9 +3,16 @@
   angular.module('webPage')
     .controller('StateController', StateController);
 
-  function StateController($state, $scope) {
+  function StateController($state, $scope, localStorageService) {
 
     this.params = $state.params;
+    this.locals = {};
+
+    const {initLocals} = $state.current.data;
+
+    if (initLocals) {
+      initLocals(this.locals, { $state, localStorageService });
+    }
 
     $scope.$watch(() => $state.current, () => {
       this.params = $state.params;
@@ -13,13 +20,13 @@
 
     _.each($state.current.data.watch, (handler, expr) => {
       $scope.$watch(expr, value => {
-        handler(value, $state);
+        handler(value, { $state, localStorageService });
       });
     });
 
     _.each($state.current.data.on, (handler, name) => {
       $scope.$on(name, (event, value) => {
-        handler(value, $state);
+        handler(value, { $state, localStorageService });
       });
     });
 
