816b3ee5b2aa5b4f580db44b8d8fb4f13cdd2f9d
Alexander Levin <alevin.ru@gmail.com>
2018-06-18 22:01:27 +0300

    WarehouseStock model

---
+++ src/app/domain/models/Warehousing/WarehouseStock.model.js
@@ -0,0 +1,23 @@
+(function () {
+
+  angular.module('Warehousing').run((Schema) => {
+
+    Schema.register({
+
+      name: 'WarehouseStock',
+      endpoint: 'Stock',
+
+      relations: {
+        belongsTo: {
+          Warehouse: {
+            localField: 'warehouse',
+            localKey: 'warehouseId',
+          },
+        },
+      },
+
+    });
+
+  });
+
+})();
