9fc263d428d149352c61c6d1b323ec966265e4d4
Alexander Levin <alevin.ru@gmail.com>
2018-06-25 10:58:18 +0300

    dateTime filter time option

--- src/app/components/filters/dateTime.js
+++ src/app/components/filters/dateTime.js
@@ -2,12 +2,12 @@
 
   angular.module('sistemium')
     .filter('dateTime', moment =>
-      function (dateTime, { seconds, date = true } = {}) {
+      function (dateTime, { seconds, date = true, time = true } = {}) {
 
         let localDate = moment.utc(dateTime).local();
 
         return localDate
-          .format(`${date ? 'DD.MM.YYг. ' : ''}в HH:mm${seconds ? ':ss' : ''}`);
+          .format(`${date ? 'DD.MM.YYг.' : ''}${time?' в HH:mm':''}${time && seconds ? ':ss' : ''}`);
 
       });
 
