285e90b50abbcc09866eecac109726982b9388c8
Alexander Levin <alevin.ru@gmail.com>
2018-07-06 17:56:48 +0300

    warehouseArticleView deletable barcode

--- src/app/domain/warehousing/articles/view/warehouseArticleView.component.js
+++ src/app/domain/warehousing/articles/view/warehouseArticleView.component.js
@@ -14,16 +14,39 @@
     });
 
   /** @ngInject */
-  function warehouseArticleViewController($scope, Schema, saControllerHelper) {
+  function warehouseArticleViewController($scope, Schema, saControllerHelper, $timeout) {
 
     const vm = saControllerHelper.setup(this, $scope);
     const { WarehouseArticle: Article } = Schema.models();
 
     vm.use({
+
       $onInit() {
         vm.rebindOne(Article, vm.articleId, 'vm.article', onArticle);
       },
+
+      removeBarcodeClick,
+
     });
+
+    function removeBarcodeClick(barcode) {
+
+      if (vm.deleteConfirm === barcode) {
+        vm.article.barcodes = _.remove(vm.barcodes, barcode);
+        vm.deleteConfirm = false;
+        vm.article.DSCreate();
+      }
+
+      vm.deleteConfirm = barcode;
+
+      $timeout(5000)
+        .then(() => {
+          if (vm.deleteConfirm === barcode) {
+            vm.deleteConfirm = false;
+          }
+        });
+
+    }
 
     function onArticle() {
 
