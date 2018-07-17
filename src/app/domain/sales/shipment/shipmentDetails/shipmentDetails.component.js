5dbf56ca52e11ad10dee81f37d5680a4e33f1936
Alexander Levin <alevin.ru@gmail.com>
2018-07-17 23:30:18 +0300

    shipmentDetails totalCostDoc

--- src/app/domain/sales/shipment/shipmentDetails/shipmentDetails.component.js
+++ src/app/domain/sales/shipment/shipmentDetails/shipmentDetails.component.js
@@ -18,13 +18,14 @@
 
   function shipmentDetailsController(Schema, $scope, saControllerHelper, $q) {
 
-    const {Shipment} = Schema.models();
+    const { Shipment, Article } = Schema.models();
 
     const vm = saControllerHelper.setup(this, $scope);
 
     vm.use({
 
-      $onInit
+      $onInit,
+      showPriceDoc: () => vm.shipment.totalCostDoc() - vm.shipment.totalCost() >= 0.01,
 
     });
 
@@ -32,9 +33,10 @@
     Init
      */
 
+
     function $onInit() {
 
-      const {Shipment} = Schema.models();
+      const { Shipment } = Schema.models();
 
       if (vm.shipmentId) {
         Shipment.find(vm.shipmentId);
@@ -56,12 +58,16 @@
 
       // TODO: subscribe to socket and do not bypassCache
 
-      return Shipment.find(shipmentId, {bypassCache})
-        .then(item => item.DSLoadRelations(['ShipmentPosition', 'Driver'], {bypassCache}))
-        .then(item => $q.all(_.map(item.positions, position => {
-          return position.DSLoadRelations()
-            .catch(_.noop);
-        })))
+      return Shipment.find(shipmentId, { bypassCache })
+        .then(item => item.DSLoadRelations(['ShipmentPosition', 'Driver'], { bypassCache }))
+        .then(item => _.map(item.positions, position => _.pick(position, ['articleId', 'article'])))
+        .then(itemArticles => {
+          const loadIds = _.filter(itemArticles, a => !a.article);
+          if (!loadIds.length) return;
+          return Article.findAll({
+            where: { id: { in: _.map(loadIds, 'articleId') } }
+          });
+        })
         .catch(e => {
           vm.error = e;
         });
