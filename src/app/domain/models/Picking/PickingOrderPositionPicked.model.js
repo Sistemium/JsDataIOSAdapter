d98d73f4f533316e918060d4c73388aeb2cf64dc
Alexander Levin <alevin.ru@gmail.com>
2018-12-06 14:34:59 +0200

    spaces

--- src/app/domain/models/Picking/PickingOrderPositionPicked.model.js
+++ src/app/domain/models/Picking/PickingOrderPositionPicked.model.js
@@ -2,46 +2,46 @@
 
 (function () {
 
-    angular.module('Models').run(function (Schema) {
+  angular.module('Models').run(function (Schema) {
 
-      Schema.register ({
+    Schema.register({
 
-        name: 'PickingOrderPositionPicked',
+      name: 'PickingOrderPositionPicked',
 
-        relations: {
-          belongsTo: {
-            PickingOrderPosition: {
-              localField: 'parent',
-              localKey: 'pickingOrderPositionId'
-            }
-          },
-          hasOne: {
-            StockBatch: {
-              localField: 'StockBatch',
-              localKey: 'stockBatchId'
-            }
+      relations: {
+        belongsTo: {
+          PickingOrderPosition: {
+            localField: 'parent',
+            localKey: 'pickingOrderPositionId'
           }
         },
-
-        // fieldTypes: {
-          // code, productionInfo
-          // volume: 'int'
-        // },
-
-        methods: {
-          boxPcs: function () {
-            const { parent } = this;
-            return parent && parent.Article && parent.Article.boxPcs(this.volume, true) || {};
-          },
-          codeLabel: function () {
-            const res = (this.code||'').match (/\d[0]*(.*)/) || [];
-
-            return res.length > 1 ? res [1] : this.code;
+        hasOne: {
+          StockBatch: {
+            localField: 'StockBatch',
+            localKey: 'stockBatchId'
           }
         }
+      },
 
-      });
+      // fieldTypes: {
+      // code, productionInfo
+      // volume: 'int'
+      // },
+
+      methods: {
+        boxPcs: function () {
+          const { parent } = this;
+          return parent && parent.Article && parent.Article.boxPcs(this.volume, true) || {};
+        },
+        codeLabel: function () {
+          const res = (this.code || '').match(/\d[0]*(.*)/) || [];
+
+          return res.length > 1 ? res [1] : this.code;
+        }
+      }
 
     });
+
+  });
 
 })();
