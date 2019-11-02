5a3b06246a4080fb4e6f5fda37880c698405b8c6
Alexander Levin <alevin.ru@gmail.com>
2019-11-02 18:15:07 +0200

    Update bottles.filter.js

--- src/app/domain/components/bottles.filter.js
+++ src/app/domain/components/bottles.filter.js
@@ -1,19 +1,19 @@
 (function () {
 
+  const dict = {
+    w1: 'бутылка',
+    w24: 'бутылки',
+    w50: 'бутылок'
+  };
+
   angular.module('core.services')
     .filter('bottles', function (Language) {
 
       return function bottlesFilter(volume) {
 
-        var dict = {
-          w1: 'бутылка',
-          w24: 'бутылки',
-          w50: 'бутылок'
-        };
-
         volume = parseInt(volume) || 0;
 
-        return volume.toFixed(0) + ' ' + dict [Language.countableState(volume)];
+        return `${volume.toFixed(0)} ${dict[Language.countableState(volume)]}`;
 
       };
 
