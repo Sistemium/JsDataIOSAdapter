225a22e23756a4e6b8559be6582227471cddc567
Alexander Levin <alevin.ru@gmail.com>
2019-11-28 13:43:16 +0200

    Create Cataloguing.service.js

---
+++ src/app/domain/sales/catalogue/Cataloguing.service.js
@@ -0,0 +1,47 @@
+(function () {
+
+  angular.module('Sales')
+    .service('Cataloguing', Cataloguing);
+
+  function Cataloguing(Schema, moment) {
+
+    const { Campaign } = Schema.models();
+
+    return {
+
+      campaignsByArticle() {
+
+        const today = moment().format();
+
+        const where = {
+          dateB: { '<=': today, },
+          dateE: { '>=': today },
+        };
+
+        return Campaign.findAll({ where })
+          .then(res => _.filter(res, ({ discount }) => discount > 0))
+          .then(variantsToArticlesHash);
+
+      },
+
+    };
+
+  }
+
+  function variantsToArticlesHash(campaigns) {
+
+    const allVariants = _.flatten(_.map(campaigns, ({ variants, id, discount }) => {
+      return _.map(variants, variant => _.assign({ campaignId: id, discount }, variant));
+    }));
+
+    const byArticles = _.flatten(_.map(allVariants, variant => {
+      return _.map(variant.articleIds, articleId => ({ articleId, variant }));
+    }));
+
+    const grouped = _.groupBy(byArticles, 'articleId');
+
+    return _.mapValues(grouped, variants => _.map(variants, 'variant'));
+
+  }
+
+})();
