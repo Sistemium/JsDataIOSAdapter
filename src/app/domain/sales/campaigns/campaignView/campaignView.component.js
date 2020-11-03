e9c6bc5b5ba8a17c28723b85c01b90f991c34abf
Alexander Levin <alevin.ru@gmail.com>
2020-11-03 12:24:37 +0200

    campaigns period

--- src/app/domain/sales/campaigns/campaignView/campaignView.component.js
+++ src/app/domain/sales/campaigns/campaignView/campaignView.component.js
@@ -26,16 +26,21 @@
         const showPictures = localStorageService.get(CAMPAIGN_SHOW_PICTURES_KEY);
         _.assign(this, {
           showPictures: showPictures !== false,
+          showPeriod: showPeriod(this.campaign),
         });
       },
 
       togglePicturesClick() {
         this.showPictures = !this.showPictures;
         localStorageService.set(CAMPAIGN_SHOW_PICTURES_KEY, this.showPictures)
-      }
+      },
 
     });
 
+    function showPeriod(campaign) {
+      return moment(campaign.dateE).add(1, 'day').date() !== 1
+        || moment(campaign.dateB).date() !== 1;
+    }
 
   }
 
