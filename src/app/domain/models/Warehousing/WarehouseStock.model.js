799d01e512ea197ad4ed4d3a2fd5f015d1a0047b
Alexander Levin <alevin.ru@gmail.com>
2021-02-12 15:33:12 +0200

    Update WarehouseStock.model.js

--- src/app/domain/models/Warehousing/WarehouseStock.model.js
+++ src/app/domain/models/Warehousing/WarehouseStock.model.js
@@ -35,12 +35,14 @@
         },
       };
 
-      return Article.findAll({ where, limit: 5000 })
+      const limit = 5000;
+
+      return Article.findAll({ where, limit })
         .then(() => {
           return WarehouseStock.findAll({
             warehouseId,
             date: moment(date).format(),
-          }, { bypassCache: true });
+          }, { bypassCache: true, limit });
         });
 
     }
