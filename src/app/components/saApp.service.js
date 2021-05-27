3c3a73ac962b2f080d3e9ae4e2ef08d31329f4aa
Alexander Levin <alevin.ru@gmail.com>
2021-05-27 20:42:00 +0300

    fix 5 age

--- src/app/components/saApp.service.js
+++ src/app/components/saApp.service.js
@@ -5,7 +5,7 @@
   function saApp($window, Schema, UUID) {
 
     const appIdKey = 'saAppId';
-    const VERSION = '0.24.10';
+    const VERSION = '0.24.11';
 
     let appId = $window.localStorage.getItem(appIdKey);
 
